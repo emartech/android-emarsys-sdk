af8c8cf3f4accb34cfafd1312a559d9ba5f0f180
Zoltan Majlath <zoltan.majlath@emarsys.com>
2018-08-01 14:24:54 +0200

    test(filter-by-shard-type-specification): QueryUsingFilterByShardType test created SUITEDEV-13744
    Co-authored-by: stoto <1409548+stoto@users.noreply.github.com>

--- core/src/androidTest/java/com/emarsys/core/shard/specification/FilterByShardTypeTest.kt
+++ core/src/androidTest/java/com/emarsys/core/shard/specification/FilterByShardTypeTest.kt
@@ -1,9 +1,12 @@
 package com.emarsys.core.shard.specification
 
+import android.support.test.InstrumentationRegistry
+import com.emarsys.core.shard.ShardModel
+import com.emarsys.core.shard.ShardModelRepository
+import com.emarsys.core.testUtil.DatabaseTestUtils
 import com.emarsys.core.testUtil.TimeoutUtils
 import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldEqual
-
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -22,6 +25,7 @@ class FilterByShardTypeTest {
 
     @Before
     fun setUp() {
+        DatabaseTestUtils.deleteCoreDatabase()
         specification = FilterByShardType(TYPE)
     }
 
@@ -31,12 +35,31 @@ class FilterByShardTypeTest {
     }
 
     @Test
-    fun testGetSql() {
+    fun testGetSqlString() {
         specification.sql shouldBeEqualTo "SELECT * FROM shard WHERE type LIKE ? ORDER BY ROWID ASC;"
     }
 
     @Test
-    fun testGetArgs() {
+    fun testGetArgsString() {
         specification.args shouldEqual arrayOf(TYPE)
+    }
+
+    @Test
+    fun testQueryUsingFilterByShardType() {
+        val context = InstrumentationRegistry.getTargetContext().applicationContext
+        val repository = ShardModelRepository(context)
+        val shardList = mutableListOf(
+                ShardModel("a1", "button_click", mapOf(), 0, 0),
+                ShardModel("a2", "button_click", mapOf(), 0, 0),
+                ShardModel("a2", "button_click", mapOf("key" to 22, "key2" to "value"), 0, 0),
+                ShardModel("a4", "not_button_click", mapOf("key" to 11, "key2" to "asdasd"), 0, 0)
+        )
+        shardList.forEach(repository::add)
+
+        val resultList = repository.query(FilterByShardType("button_click"))
+
+        val expectedList = shardList.filter { x -> (x.type == "button_click") }
+
+        resultList shouldEqual expectedList
     }
 }
