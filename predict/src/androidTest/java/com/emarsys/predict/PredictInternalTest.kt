dbc677e963a5f7b63e2e39ef72bb9f3fc3fc7ad8
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-08-28 11:29:56 +0200

    feat(predict-internal): add setCustomer method SUITEDEV-13721
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

---
+++ predict/src/androidTest/java/com/emarsys/predict/PredictInternalTest.kt
@@ -0,0 +1,45 @@
+package com.emarsys.predict
+
+import com.emarsys.core.storage.KeyValueStore
+import com.emarsys.predict.testUtil.TimeoutUtils
+import org.junit.Before
+
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+class PredictInternalTest {
+
+    @Rule
+    @JvmField
+    var timeout = TimeoutUtils.getTimeoutRule()
+
+    private lateinit var mockKeyValueStore: KeyValueStore
+    private lateinit var predictInternal: PredictInternal
+
+    @Before
+    fun init() {
+        mockKeyValueStore = mock(KeyValueStore::class.java)
+        predictInternal = PredictInternal(mockKeyValueStore)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testConstructor_keyValueStore_shouldNotBeNull() {
+        PredictInternal(null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testSetCustomer_customerId_mustNotBeNull() {
+        predictInternal.setCustomer(null)
+    }
+
+    @Test
+    fun testSetCustomer_shouldPersistsWithKeyValueStore() {
+        val customerId = "customerId"
+
+        predictInternal.setCustomer(customerId)
+
+        verify(mockKeyValueStore).putString("predict_customerId", customerId)
+    }
+}
--- predict/src/main/java/com/emarsys/predict/PredictInternal.java
+++ predict/src/main/java/com/emarsys/predict/PredictInternal.java
@@ -1,11 +1,20 @@
 package com.emarsys.predict;
 
+import com.emarsys.core.storage.KeyValueStore;
 import com.emarsys.core.util.Assert;
 
 public class PredictInternal {
 
-    public void setTag(String tag){
-        Assert.notNull(tag, "Tag must not be null!");
+    private KeyValueStore keyValueStore;
+
+    public PredictInternal(KeyValueStore keyValueStore) {
+        Assert.notNull(keyValueStore, "KeyValueStore must not be null!");
+
+        this.keyValueStore = keyValueStore;
     }
 
+    public void setCustomer(String customerId) {
+        Assert.notNull(customerId, "CustomerId must not be null!");
+        keyValueStore.putString("predict_customerId", customerId);
+    }
 }
