501f4473cea286432c15f02ae358b7a2f20cce68
megamegax <mhunyady@emarsys.com>
2018-09-21 14:09:10 +0200

    chore(kotlin-test): add kotlin-test as testing framework SUITEDEV-14515
    Co-authored-by: pjozsef <9980358+pjozsef@users.noreply.github.com>

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,5 +1,6 @@
 object Versions {
     const val kotlin_version = "1.2.70"
+    const val kotlin_test_version = "3.1.10"
     const val mockito_version = "2.18.3"
     const val espresso_version = "3.0.2"
     const val firebase_core_version = "16.0.3"
@@ -19,6 +20,7 @@ object Libs {
 
 object TestLibs {
     val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}"
+    val kotlin_test = "io.kotlintest:kotlintest-runner-junit4:${Versions.kotlin_test_version}"
     val mockito_android = "org.mockito:mockito-android:${Versions.mockito_version}"
     val espresso_core = "com.android.support.test.espresso:espresso-core:${Versions.espresso_version}"
     val support_test_runner = "com.android.support.test:runner:${Versions.support_test_version}"
