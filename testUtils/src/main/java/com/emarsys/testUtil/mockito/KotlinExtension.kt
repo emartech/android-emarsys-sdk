de147c1c858af7f10597fc0fe35175bf51f93624
stoto <pstojcsics@emarsys.com>
2018-10-12 11:03:19 +0200

    test(kotlin-mock): introduce whenever to use instead of `when` SUITEDEV-14049
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

---
+++ testUtils/src/main/java/com/emarsys/testUtil/mockito/KotlinExtension.kt
@@ -0,0 +1,7 @@
+package com.emarsys.testUtil.mockito
+
+import org.mockito.Mockito.`when`
+import org.mockito.stubbing.OngoingStubbing
+
+
+fun <T>whenever(mock:T): OngoingStubbing<T> = `when`(mock)
