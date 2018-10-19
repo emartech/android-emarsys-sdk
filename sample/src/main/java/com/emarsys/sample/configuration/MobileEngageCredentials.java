736d66a8217f8d9470fa8df2612e1854acb6fe81
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-10-19 14:33:56 +0200

    feat(sample): extract ME credentials SUITEDEV-14590
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

---
+++ sample/src/main/java/com/emarsys/sample/configuration/MobileEngageCredentials.java
@@ -0,0 +1,10 @@
+package com.emarsys.sample.configuration;
+
+public class MobileEngageCredentials {
+    public static String INTEGRATION_APPLICATION_CODE = "14C19-A121F";
+    public static String INTEGRATION_APPLICATION_PASSWORD = "PaNkfOD90AVpYimMBuZopCpm8OWCrREu";
+
+    public static String PROD_APPLICATION_CODE = "EMSA1-927A9";
+    public static String PROD_APPLICATION_PASSWORD = "kQ8qXPzCuzzXZ9jTnuRT09zcv6aKsYf0";
+
+}
