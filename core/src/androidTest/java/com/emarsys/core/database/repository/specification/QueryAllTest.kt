57d2f8c21bc22476c054f6e9b75256ea676391ad
megamegax <mhunyady@emarsys.com>
2018-11-09 10:25:43 +0100

    test(query-all): add test for testing specification on db SUITEDEV-14759

--- core/src/androidTest/java/com/emarsys/core/database/repository/specification/QueryAllTest.kt
+++ core/src/androidTest/java/com/emarsys/core/database/repository/specification/QueryAllTest.kt
@@ -1,5 +1,10 @@
 package com.emarsys.core.database.repository.specification
 
+import android.support.test.InstrumentationRegistry
+import com.emarsys.core.database.helper.CoreDbHelper
+import com.emarsys.core.shard.ShardModel
+import com.emarsys.core.shard.ShardModelRepository
+import com.emarsys.testUtil.DatabaseTestUtils
 import com.emarsys.testUtil.TimeoutUtils
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
@@ -17,6 +22,7 @@ class QueryAllTest {
 
     @Before
     fun init() {
+        DatabaseTestUtils.deleteCoreDatabase()
         specification = QueryAll("table")
     }
 
@@ -36,6 +42,26 @@ class QueryAllTest {
     @Test
     fun testGetArgs() {
         assertNull(specification.args)
+    }
+
+    @Test
+    fun testQueryUsingQueryAll() {
+        specification = QueryAll("shard")
+
+        val context = InstrumentationRegistry.getTargetContext().applicationContext
+        val coreDbHelper = CoreDbHelper(context, mapOf())
+        val repository = ShardModelRepository(coreDbHelper)
+        val expectedList = mutableListOf(
+                ShardModel("a1", "button_click", mapOf(), 0, 0),
+                ShardModel("a2", "button_click", mapOf(), 0, 0),
+                ShardModel("a2", "button_click", mapOf("key" to 22, "key2" to "value"), 0, 0),
+                ShardModel("a4", "not_button_click", mapOf("key" to 11, "key2" to "asdasd"), 0, 0)
+        )
+        expectedList.forEach(repository::add)
+
+        val resultList = repository.query(QueryAll("shard"))
+
+        assertEquals(expectedList, resultList)
     }
 
 }
