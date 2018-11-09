5b90be90707d8a4007f4ec920a87cfc17e009d0c
megamegax <mhunyady@emarsys.com>
2018-11-09 10:42:37 +0100

    test(query-newest-request-model): add test for testing specification on db SUITEDEV-14759

--- core/src/androidTest/java/com/emarsys/core/database/repository/specification/QueryNewestRequestModelTest.kt
+++ core/src/androidTest/java/com/emarsys/core/database/repository/specification/QueryNewestRequestModelTest.kt
@@ -1,5 +1,11 @@
 package com.emarsys.core.database.repository.specification
 
+import android.support.test.InstrumentationRegistry
+import com.emarsys.core.database.helper.CoreDbHelper
+import com.emarsys.core.provider.timestamp.TimestampProvider
+import com.emarsys.core.provider.uuid.UUIDProvider
+import com.emarsys.core.request.model.RequestModel
+import com.emarsys.core.request.model.RequestModelRepository
 import com.emarsys.core.request.model.specification.QueryNewestRequestModel
 import com.emarsys.testUtil.DatabaseTestUtils
 import com.emarsys.testUtil.TimeoutUtils
@@ -33,5 +39,28 @@ class QueryNewestRequestModelTest {
     @Test
     fun testGetArgs() {
         specification.args shouldBe null
+    }
+
+    @Test
+    fun testQueryUsingQueryNewestRequestModel() {
+        specification = QueryNewestRequestModel()
+        val timestampProvider = TimestampProvider()
+        val uuidProvider = UUIDProvider()
+
+        val context = InstrumentationRegistry.getTargetContext().applicationContext
+        val coreDbHelper = CoreDbHelper(context, mapOf())
+        val repository = RequestModelRepository(coreDbHelper)
+
+        val expectedRequestModel = RequestModel.Builder(timestampProvider, uuidProvider).url("https://emarsys.com/1").build()
+        val requestModel1 = RequestModel.Builder(timestampProvider, uuidProvider).url("https://emarsys.com/2").build()
+        val requestModel2 = RequestModel.Builder(timestampProvider, uuidProvider).url("https://emarsys.com/3").build()
+
+        repository.add(expectedRequestModel)
+        repository.add(requestModel1)
+        repository.add(requestModel2)
+
+        val resultList = repository.query(specification)
+
+        resultList[0] shouldBe expectedRequestModel
     }
 }
