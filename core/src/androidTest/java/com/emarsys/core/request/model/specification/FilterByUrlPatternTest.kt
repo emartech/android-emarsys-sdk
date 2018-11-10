2d39468131c1f4d8ba9ceb4a465fdcbd3b1d2972
megamegax <mhunyady@emarsys.com>
2018-11-10 10:11:59 +0100

    test(filter-by-urlpattern): add test for testing specification on db SUITEDEV-14759

--- core/src/androidTest/java/com/emarsys/core/request/model/specification/FilterByUrlPatternTest.kt
+++ core/src/androidTest/java/com/emarsys/core/request/model/specification/FilterByUrlPatternTest.kt
@@ -1,6 +1,13 @@
 package com.emarsys.core.request.model.specification
 
+import android.support.test.InstrumentationRegistry
+import com.emarsys.core.database.helper.CoreDbHelper
+import com.emarsys.core.provider.timestamp.TimestampProvider
+import com.emarsys.core.provider.uuid.UUIDProvider
+import com.emarsys.core.request.model.RequestModel
+import com.emarsys.core.request.model.RequestModelRepository
 import com.emarsys.testUtil.TimeoutUtils
+import io.kotlintest.matchers.collections.shouldContainAll
 import io.kotlintest.shouldBe
 import org.junit.Before
 import org.junit.Rule
@@ -34,5 +41,29 @@ class FilterByUrlPatternTest {
     @Test
     fun testGetArs() {
         specification.args shouldBe arrayOf(pattern)
+    }
+
+    @Test
+    fun testQueryUsingFilterByUrlPattern() {
+        specification = FilterByUrlPattern("https://emarsys.com/%")
+        val timestampProvider = TimestampProvider()
+        val uuidProvider = UUIDProvider()
+
+        val context = InstrumentationRegistry.getTargetContext().applicationContext
+        val coreDbHelper = CoreDbHelper(context, mapOf())
+        val repository = RequestModelRepository(coreDbHelper)
+
+        val expectedRequestModel = RequestModel.Builder(timestampProvider, uuidProvider).url("https://emarsys.com/1").build()
+        val requestModel1 = RequestModel.Builder(timestampProvider, uuidProvider).url("https://google.com/2").build()
+        val expectedRequestModel2 = RequestModel.Builder(timestampProvider, uuidProvider).url("https://emarsys.com/3").build()
+
+        repository.add(expectedRequestModel)
+        repository.add(requestModel1)
+        repository.add(expectedRequestModel2)
+
+        val resultList = repository.query(specification)
+
+        resultList.size shouldBe 2
+        resultList.shouldContainAll(expectedRequestModel, expectedRequestModel2)
     }
 }
