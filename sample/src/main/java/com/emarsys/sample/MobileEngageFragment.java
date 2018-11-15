1ee1a3e9209764fad35460b5dc9eb70c76b2d8f7
megamegax <mhunyady@emarsys.com>
2018-11-15 11:18:36 +0100

    feat(predict): introduce trackCartItem, trackPurchase features to sample app SUITEDEV-14210
    Co-authored-by: stoto <1409548+stoto@users.noreply.github.com>

--- sample/src/main/java/com/emarsys/sample/MobileEngageFragment.java
+++ sample/src/main/java/com/emarsys/sample/MobileEngageFragment.java
@@ -5,13 +5,10 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -20,10 +17,7 @@ import android.widget.Toast;
 import com.emarsys.Emarsys;
 import com.emarsys.core.api.ResponseErrorException;
 import com.emarsys.core.api.result.CompletionListener;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -51,26 +45,23 @@ public class MobileEngageFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_mobile_engage, container, false);
 
-        final CompletionListener completionListener = new CompletionListener() {
-            @Override
-            public void onCompleted(@Nullable Throwable errorCause) {
-                String message;
-                if (errorCause == null) {
-                    message = "OK";
-                    Log.i(TAG, message);
-                } else {
-                    Log.e(TAG, errorCause.getMessage(), errorCause);
-                    StringBuilder sb = new StringBuilder();
-                    if (errorCause instanceof ResponseErrorException) {
-                        ResponseErrorException mee = (ResponseErrorException) errorCause;
-                        sb.append(mee.getStatusCode());
-                        sb.append(" - ");
-                    }
-                    sb.append(errorCause.getMessage());
-                    message = sb.toString();
+        final CompletionListener completionListener = errorCause -> {
+            String message;
+            if (errorCause == null) {
+                message = "OK";
+                Log.i(TAG, message);
+            } else {
+                Log.e(TAG, errorCause.getMessage(), errorCause);
+                StringBuilder sb = new StringBuilder();
+                if (errorCause instanceof ResponseErrorException) {
+                    ResponseErrorException mee = (ResponseErrorException) errorCause;
+                    sb.append(mee.getStatusCode());
+                    sb.append(" - ");
                 }
-                statusLabel.append(message);
+                sb.append(errorCause.getMessage());
+                message = sb.toString();
             }
+            statusLabel.append(message);
         };
         statusLabel = root.findViewById(R.id.mobileEngageStatusLabel);
 
@@ -79,115 +70,89 @@ public class MobileEngageFragment extends BaseFragment {
         eventAttributes = root.findViewById(R.id.eventAttributes);
         messageId = root.findViewById(R.id.messageId);
 
-        root.findViewById(R.id.appLoginAnonymous).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Emarsys.setAnonymousCustomer(completionListener);
-                handleRequestSent("Anonymous login: ");
-            }
+        root.findViewById(R.id.appLoginAnonymous).setOnClickListener(v -> {
+            Emarsys.setAnonymousCustomer(completionListener);
+            handleRequestSent("Anonymous login: ");
         });
 
-        root.findViewById(R.id.appLogin).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String id = contactFieldValue.getText().toString();
-                Emarsys.setCustomer(id, new CompletionListener() {
-                    @Override
-                    public void onCompleted(@Nullable Throwable errorCause) {
-                        completionListener.onCompleted(errorCause);
-                        if (errorCause != null) {
-                            ((MainActivity) getActivity()).updateBadgeCount();
-                        }
-                    }
-                });
-                handleRequestSent("Login: ");
+        root.findViewById(R.id.appLogin).setOnClickListener(v -> {
+            String id = contactFieldValue.getText().toString();
+            if (id.isEmpty()) {
+                id = contactFieldValue.getHint().toString();
             }
-        });
-
-        root.findViewById(R.id.appLogout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Emarsys.clearCustomer(completionListener);
-                handleRequestSent("Logout: ");
-            }
-        });
-
-        root.findViewById(R.id.customEvent).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = eventName.getText().toString();
-                String attributesString = eventAttributes.getText().toString();
-
-                Map<String, String> attributes = null;
-                if (!attributesString.isEmpty()) {
-                    try {
-                        attributes = new HashMap<>();
-                        JSONObject json = new JSONObject(attributesString);
-                        Iterator<String> keys = json.keys();
-                        while (keys.hasNext()) {
-                            String key = keys.next();
-                            attributes.put(key, json.getString(key));
-                        }
-                    } catch (JSONException e) {
-                        Log.w(TAG, "Event attributes edittext content is not a valid JSON!");
-                    }
+            Emarsys.setCustomer(id, errorCause -> {
+                completionListener.onCompleted(errorCause);
+                if (errorCause != null) {
+                    ((MainActivity) getActivity()).updateBadgeCount();
                 }
-
-                Emarsys.trackCustomEvent(name, attributes, completionListener);
-                handleRequestSent("Custom event: ");
-            }
+            });
+            handleRequestSent("Login: ");
         });
 
-        root.findViewById(R.id.messageOpen).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                String id = messageId.getText().toString();
-                Bundle payload = new Bundle();
-                payload.putString("key1", "value1");
-                payload.putString("u", String.format("{\"sid\": \"%s\"}", id));
-                intent.putExtra("payload", payload);
-                Emarsys.Push.trackMessageOpen(intent, completionListener);
-                handleRequestSent("Message open: ");
-            }
+        root.findViewById(R.id.appLogout).setOnClickListener(v -> {
+            Emarsys.clearCustomer(completionListener);
+            handleRequestSent("Logout: ");
         });
 
-        root.<Switch>findViewById(R.id.doNotDisturb).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Emarsys.InApp.pause();
-                } else {
-                    Emarsys.InApp.resume();
+        root.findViewById(R.id.customEvent).setOnClickListener(v -> {
+            String name = eventName.getText().toString();
+            String attributesString = eventAttributes.getText().toString();
+
+            Map<String, String> attributes = null;
+            if (!attributesString.isEmpty()) {
+                try {
+                    attributes = new HashMap<>();
+                    JSONObject json = new JSONObject(attributesString);
+                    Iterator<String> keys = json.keys();
+                    while (keys.hasNext()) {
+                        String key = keys.next();
+                        attributes.put(key, json.getString(key));
+                    }
+                } catch (JSONException e) {
+                    Log.w(TAG, "Event attributes edittext content is not a valid JSON!");
                 }
             }
+
+            Emarsys.trackCustomEvent(name, attributes, completionListener);
+            handleRequestSent("Custom event: ");
         });
 
-        root.findViewById(R.id.pushToken).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View view) {
-                FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(
-                        getActivity(),
-                        new OnCompleteListener<InstanceIdResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<InstanceIdResult> task) {
-                                ClipboardManager clipboard = (ClipboardManager) view.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-                                String pushToken = task.getResult().getToken();
-                                String toastMessage = pushToken;
-                                if (clipboard != null) {
-                                    ClipData clip = ClipData.newPlainText("pushtoken", pushToken);
-                                    clipboard.setPrimaryClip(clip);
-                                    toastMessage = "Copied: " + toastMessage;
-                                }
-                                Toast.makeText(
-                                        view.getContext(),
-                                        toastMessage,
-                                        Toast.LENGTH_LONG).show();
-                            }
-                        }
-                );
+        root.findViewById(R.id.messageOpen).setOnClickListener(v -> {
+            Intent intent = new Intent();
+            String id = messageId.getText().toString();
+            Bundle payload = new Bundle();
+            payload.putString("key1", "value1");
+            payload.putString("u", String.format("{\"sid\": \"%s\"}", id));
+            intent.putExtra("payload", payload);
+            Emarsys.Push.trackMessageOpen(intent, completionListener);
+            handleRequestSent("Message open: ");
+        });
+
+        root.<Switch>findViewById(R.id.doNotDisturb).setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                Emarsys.InApp.pause();
+            } else {
+                Emarsys.InApp.resume();
             }
         });
+
+        root.findViewById(R.id.pushToken).setOnClickListener(view -> FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(
+                getActivity(),
+                task -> {
+                    ClipboardManager clipboard = (ClipboardManager) view.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                    String pushToken = task.getResult().getToken();
+                    String toastMessage = pushToken;
+                    if (clipboard != null) {
+                        ClipData clip = ClipData.newPlainText("pushtoken", pushToken);
+                        clipboard.setPrimaryClip(clip);
+                        toastMessage = "Copied: " + toastMessage;
+                    }
+                    Toast.makeText(
+                            view.getContext(),
+                            toastMessage,
+                            Toast.LENGTH_LONG).show();
+                }
+        ));
 
         return root;
     }
--- sample/src/main/java/com/emarsys/sample/PredictFragment.java
+++ sample/src/main/java/com/emarsys/sample/PredictFragment.java
@@ -7,12 +7,21 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.emarsys.Emarsys;
+import com.emarsys.predict.api.model.CartItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class PredictFragment extends BaseFragment {
 
     private EditText itemView;
     private EditText categoryView;
     private EditText searchTerm;
+    private EditText orderId;
+    private EditText cartItemsContainer;
+    private List<CartItem> cartItems = new ArrayList<>();
+    private Random random = new Random();
 
     @Override
     public String getName() {
@@ -26,6 +35,8 @@ public class PredictFragment extends BaseFragment {
         itemView = root.findViewById(R.id.itemView);
         categoryView = root.findViewById(R.id.categoryView);
         searchTerm = root.findViewById(R.id.searchTerm);
+        orderId = root.findViewById(R.id.orderId);
+        cartItemsContainer = root.findViewById(R.id.cartItems);
 
         root.findViewById(R.id.trackItemView).setOnClickListener(view -> {
             String itemId = itemView.getText().toString();
@@ -33,18 +44,77 @@ public class PredictFragment extends BaseFragment {
                 Emarsys.Predict.trackItemView(itemId);
             }
         });
+
         root.findViewById(R.id.trackCategoryView).setOnClickListener(view -> {
             String categoryText = categoryView.getText().toString();
             if (!categoryText.isEmpty()) {
-                Emarsys.Predict.trackItemView(categoryView.getText().toString());
+                Emarsys.Predict.trackCategoryView(categoryView.getText().toString());
             }
         });
+
         root.findViewById(R.id.trackSearchTerm).setOnClickListener(view -> {
             String searchTermText = searchTerm.getText().toString();
             if (!searchTermText.isEmpty()) {
-                Emarsys.Predict.trackItemView(searchTerm.getText().toString());
+                Emarsys.Predict.trackSearchTerm(searchTerm.getText().toString());
+            }
+        });
+
+        root.findViewById(R.id.addCartItem).setOnClickListener(view -> {
+            CartItem cartItem = generateCartItem();
+            cartItems.add(cartItem);
+            cartItemsContainer.getText().append(cartItem.toString());
+
+        });
+
+        root.findViewById(R.id.trackCartItems).setOnClickListener(view -> {
+            if (!cartItems.isEmpty()) {
+                Emarsys.Predict.trackCart(cartItems);
+            }
+        });
+
+        root.findViewById(R.id.trackPurchase).setOnClickListener(view -> {
+            String orderIdText = orderId.getText().toString();
+            if (!cartItems.isEmpty()) {
+                Emarsys.Predict.trackPurchase(orderIdText, cartItems);
             }
         });
         return root;
+    }
+
+    private CartItem generateCartItem() {
+        return new SampleCartItem(getRandomItemId(), 1 + random.nextInt(99), 1 + random.nextInt(5));
+    }
+
+    private String getRandomItemId() {
+        String identifiers[] = new String[]{"2156",
+                "2157",
+                "2158",
+                "2159",
+                "2160",
+                "2161",
+                "2163",
+                "2164",
+                "2165",
+                "2166",
+                "2167",
+                "2182",
+                "2169",
+                "2170",
+                "2171",
+                "2172",
+                "2173",
+                "2174",
+                "2175",
+                "2176",
+                "2177",
+                "2178",
+                "2179",
+                "2180",
+                "2181",
+                "2168",
+                "2183",
+                "2162",
+                "2184"};
+        return identifiers[random.nextInt(identifiers.length)];
     }
 }
