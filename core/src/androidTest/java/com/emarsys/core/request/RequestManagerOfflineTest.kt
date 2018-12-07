d6a6b612503a1c6a03f3b73299384462be78ca06
megamegax <mhunyady@emarsys.com>
2018-12-07 15:26:42 +0100

    test(offline): convert test to kotlin SUITEDEV-15537
    Co-authored-by: LaszloOriEmarsys <24588073+LaszloOriEmarsys@users.noreply.github.com>

---
+++ core/src/androidTest/java/com/emarsys/core/request/RequestManagerOfflineTest.kt
@@ -0,0 +1,275 @@
+package com.emarsys.core.request
+
+import android.os.Handler
+import android.os.Looper
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.emarsys.core.Registry
+import com.emarsys.core.api.result.CompletionListener
+import com.emarsys.core.concurrency.CoreSdkHandlerProvider
+import com.emarsys.core.connection.ConnectionState
+import com.emarsys.core.database.helper.CoreDbHelper
+import com.emarsys.core.database.repository.Repository
+import com.emarsys.core.database.repository.SqlSpecification
+import com.emarsys.core.database.repository.specification.Everything
+import com.emarsys.core.fake.FakeCompletionHandler
+import com.emarsys.core.fake.FakeConnectionWatchDog
+import com.emarsys.core.fake.FakeRestClient
+import com.emarsys.core.provider.timestamp.TimestampProvider
+import com.emarsys.core.provider.uuid.UUIDProvider
+import com.emarsys.core.request.model.RequestMethod
+import com.emarsys.core.request.model.RequestModel
+import com.emarsys.core.request.model.RequestModelRepository
+import com.emarsys.core.request.model.specification.FilterByRequestId
+import com.emarsys.core.shard.ShardModel
+import com.emarsys.core.shard.ShardModelRepository
+import com.emarsys.core.worker.DefaultWorker
+import com.emarsys.core.worker.Worker
+import com.emarsys.testUtil.DatabaseTestUtils
+import com.emarsys.testUtil.InstrumentationRegistry
+import com.emarsys.testUtil.TimeoutUtils
+import io.kotlintest.matchers.beEmpty
+import io.kotlintest.should
+import io.kotlintest.shouldBe
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertFalse
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import java.io.IOException
+import java.util.*
+import java.util.concurrent.CountDownLatch
+
+@RunWith(AndroidJUnit4::class)
+class RequestManagerOfflineTest {
+    companion object {
+        val URL = "https://www.host.com/"
+    }
+
+    @Rule
+    @JvmField
+    var timeout: TestRule = TimeoutUtils.timeoutRule
+
+    private lateinit var connectionStates: Array<Boolean>
+    private lateinit var requestResults: Array<Any>
+    private lateinit var requestModels: Array<RequestModel>
+    private var watchDogCountDown: Int = 0
+    private var completionHandlerCountDown: Int = 0
+
+    private lateinit var watchDogLatch: CountDownLatch
+    private lateinit var watchDog: FakeConnectionWatchDog
+    private lateinit var requestRepository: Repository<RequestModel, SqlSpecification>
+    private lateinit var shardRepository: Repository<ShardModel, SqlSpecification>
+    private lateinit var completionLatch: CountDownLatch
+    private lateinit var completionHandler: FakeCompletionHandler
+    private lateinit var manager: RequestManager
+    private lateinit var fakeRestClient: RestClient
+    private lateinit var provider: CoreSdkHandlerProvider
+    private lateinit var coreSdkHandler: Handler
+    private lateinit var uiHandler: Handler
+    private lateinit var worker: Worker
+
+
+    @Before
+    fun setup() {
+        DatabaseTestUtils.deleteCoreDatabase()
+
+        uiHandler = Handler(Looper.getMainLooper())
+    }
+
+    @After
+    fun tearDown() {
+        coreSdkHandler.looper.quit()
+        DatabaseTestUtils.deleteCoreDatabase()
+    }
+
+    @Test
+    fun test_online_offline_online() {
+        connectionStates = arrayOf(true, false, true)
+        requestResults = arrayOf(200, 200)
+        requestModels = arrayOf(normal(1), normal(2))
+        watchDogCountDown = 2
+        completionHandlerCountDown = 1
+
+        prepareTestCaseAndWait()
+
+        assertFalse(requestRepository.isEmpty)
+        completionHandler.latch = CountDownLatch(1)
+
+        uiHandler.post { watchDog.connectionChangeListener.onConnectionChanged(ConnectionState.CONNECTED, true) }
+
+        completionHandler.latch.await()
+
+        completionHandler.onSuccessCount shouldBe 2
+        requestRepository.query(Everything()) should beEmpty()
+    }
+
+    @Test
+    fun test_alwaysOnline() {
+        connectionStates = arrayOf(true)
+        requestResults = arrayOf(200, 200, 200)
+        requestModels = arrayOf(normal(1), normal(2), normal(3))
+        watchDogCountDown = 3
+        completionHandlerCountDown = 3
+
+        prepareTestCaseAndWait()
+
+        completionHandler.onSuccessCount shouldBe 3
+        requestRepository.query(Everything()) should beEmpty()
+    }
+
+    @Test
+    fun test_alwaysOnline_withExpiredRequests() {
+        connectionStates = arrayOf(true)
+        requestResults = arrayOf(200, 200, 200)
+        requestModels = arrayOf(normal(1), expired(1), normal(2), expired(2), expired(3), expired(4), normal(3))
+        watchDogCountDown = 3
+        completionHandlerCountDown = 7
+
+        prepareTestCaseAndWait()
+
+        completionHandler.onSuccessCount shouldBe 3
+        completionHandler.onErrorCount shouldBe 4
+
+        requestRepository.query(Everything()) should beEmpty()
+    }
+
+    @Test
+    fun test_alwaysOffline() {
+        connectionStates = arrayOf(false)
+        requestResults = arrayOf(200, 300, 200)
+
+        val normal1 = normal(1)
+        val normal2 = normal(2)
+        val normal3 = normal(3)
+        requestModels = arrayOf(normal1, normal2, normal3)
+        watchDogCountDown = 3
+        completionHandlerCountDown = 0
+
+        prepareTestCaseAndWait()
+
+        completionHandler.onSuccessCount shouldBe 0
+        completionHandler.onErrorCount shouldBe 0
+        requestRepository.isEmpty shouldBe false
+
+        val result = requestRepository.query(Everything())
+        assertEquals(Arrays.asList(*requestModels), result)
+    }
+
+    @Test
+    fun test_4xx_doesNotStopQueue() {
+        connectionStates = arrayOf(true)
+        requestResults = arrayOf(200, 400, 200)
+        requestModels = arrayOf(normal(1), normal(2), normal(3))
+        watchDogCountDown = 3
+        completionHandlerCountDown = 3
+
+        prepareTestCaseAndWait()
+
+        completionHandler.onSuccessCount shouldBe 2
+        completionHandler.onErrorCount shouldBe 1
+
+        requestRepository.query(Everything()) should beEmpty()
+    }
+
+    @Test
+    fun test_408_stopsQueue() {
+        connectionStates = arrayOf(true)
+        requestResults = arrayOf(200, 408, 200)
+        requestModels = arrayOf(normal(1), normal(2), normal(3))
+        watchDogCountDown = 2
+        completionHandlerCountDown = 1
+
+        prepareTestCaseAndWait()
+
+        completionHandler.onSuccessCount shouldBe 1
+        completionHandler.onErrorCount shouldBe 0
+        requestRepository.isEmpty shouldBe false
+    }
+
+    @Test
+    fun test_5xx_stopsQueue() {
+        connectionStates = arrayOf(true)
+        requestResults = arrayOf(200, 500, 200)
+        requestModels = arrayOf(normal(1), normal(2), normal(3))
+        watchDogCountDown = 2
+        completionHandlerCountDown = 1
+
+        prepareTestCaseAndWait()
+
+        completionHandler.onSuccessCount shouldBe 1
+        completionHandler.onErrorCount shouldBe 0
+        requestRepository.isEmpty shouldBe false
+    }
+
+    @Test
+    fun test_exception_stopsQueue() {
+        connectionStates = arrayOf(true)
+        requestResults = arrayOf(200, 300, 200, IOException())
+        val lastNormal = normal(4)
+        requestModels = arrayOf(normal(1), normal(2), normal(3), lastNormal)
+        watchDogCountDown = 4
+        completionHandlerCountDown = 3
+
+        prepareTestCaseAndWait()
+
+        completionHandler.onSuccessCount shouldBe 3
+        completionHandler.onErrorCount shouldBe 0
+        requestRepository.isEmpty shouldBe false
+
+        requestRepository.remove(FilterByRequestId(lastNormal))
+
+        requestRepository.query(Everything()) should beEmpty()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun prepareTestCaseAndWait() {
+        watchDogLatch = CountDownLatch(watchDogCountDown)
+        watchDog = FakeConnectionWatchDog(watchDogLatch, *connectionStates)
+
+        val coreDbHelper = CoreDbHelper(InstrumentationRegistry.getTargetContext(), HashMap())
+        requestRepository = RequestModelRepository(coreDbHelper)
+        shardRepository = ShardModelRepository(coreDbHelper)
+
+        completionLatch = CountDownLatch(completionHandlerCountDown)
+        completionHandler = FakeCompletionHandler(completionLatch)
+
+        fakeRestClient = FakeRestClient(*requestResults)
+
+        provider = CoreSdkHandlerProvider()
+        coreSdkHandler = provider.provideHandler()
+
+        worker = DefaultWorker(requestRepository, watchDog, uiHandler, coreSdkHandler, completionHandler, fakeRestClient)
+
+        manager = RequestManager(coreSdkHandler, requestRepository, shardRepository, worker, fakeRestClient, mock(Registry::class.java) as Registry<RequestModel, CompletionListener>)
+
+        uiHandler.post {
+            for (i in requestModels.indices) {
+                manager.submit(requestModels[i], null)
+            }
+        }
+
+        watchDogLatch.await()
+        completionLatch.await()
+    }
+
+    private fun normal(orderId: Int): RequestModel {
+        val timestampProvider = TimestampProvider()
+        val uuidProvider = UUIDProvider()
+        return RequestModel.Builder(timestampProvider, uuidProvider).url(URL + "normal/" + orderId).method(RequestMethod.GET).ttl(60000).build()
+    }
+
+    private fun expired(orderId: Int): RequestModel {
+        return RequestModel(
+                URL + "expired/" + orderId,
+                RequestMethod.GET,
+                HashMap(),
+                HashMap(),
+                System.currentTimeMillis() - 5000,
+                100,
+                UUIDProvider().provideId())
+    }
+}
