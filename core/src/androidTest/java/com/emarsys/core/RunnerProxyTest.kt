c8d8439bafcb24f6137673c5472dcfdc0ce00884
megamegax <mhunyady@emarsys.com>
2019-02-01 12:09:55 +0100

    test(log): add test case for crash logging in RunnerProxy SUITEDEV-16174
    Co-authored-by: LaszloOriEmarsys <24588073+LaszloOriEmarsys@users.noreply.github.com>

--- core/src/androidTest/java/com/emarsys/core/RunnerProxyTest.kt
+++ core/src/androidTest/java/com/emarsys/core/RunnerProxyTest.kt
@@ -1,10 +1,16 @@
 package com.emarsys.core
 
+import com.emarsys.core.di.DependencyContainer
+import com.emarsys.core.di.DependencyInjection
+import com.emarsys.core.util.log.Logger
 import com.emarsys.testUtil.TimeoutUtils
+import com.emarsys.testUtil.mockito.MockitoTestUtils.whenever
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
@@ -15,10 +21,23 @@ class RunnerProxyTest {
     val timeout: TestRule = TimeoutUtils.timeoutRule
 
     lateinit var runnerProxy: RunnerProxy
+    lateinit var mockLogger: Logger
 
     @Before
     fun setUp() {
+        mockLogger = mock(Logger::class.java)
+
+        val dependencyContainer = mock(DependencyContainer::class.java).apply {
+            whenever(logger).thenReturn(mockLogger)
+        }
+
+        DependencyInjection.setup(dependencyContainer)
         runnerProxy = RunnerProxy()
+    }
+
+    @After
+    fun tearDown() {
+        DependencyInjection.tearDown()
     }
 
     @Test
@@ -37,5 +56,18 @@ class RunnerProxyTest {
         runnerProxy.logException(callable)
 
         verify(callable).call()
+    }
+
+    @Test
+    fun testLogException_shouldLogCrashes() {
+        val runnable = mock(Runnable::class.java).apply {
+            whenever(run()).thenThrow(RuntimeException())
+        }
+        try {
+            runnerProxy.logException(runnable)
+        } catch (exception: java.lang.RuntimeException) {
+
+        }
+        verify(mockLogger).persistLog(any())
     }
 }
