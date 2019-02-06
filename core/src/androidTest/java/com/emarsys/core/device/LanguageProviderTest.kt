9642a761500babcd05b316a8004eb535ec293585
megamegax <mhunyady@emarsys.com>
2019-02-06 15:32:10 +0100

    feat(language-provider): add support for lower api levels SUITEDEV-16272
    Co-authored-by: pjozsef <9980358+pjozsef@users.noreply.github.com>

--- core/src/androidTest/java/com/emarsys/core/device/LanguageProviderTest.kt
+++ core/src/androidTest/java/com/emarsys/core/device/LanguageProviderTest.kt
@@ -1,6 +1,6 @@
 package com.emarsys.core.device
 
-import android.os.Build.VERSION_CODES.LOLLIPOP
+import android.os.Build.VERSION_CODES.P
 import androidx.test.filters.SdkSuppress
 import com.emarsys.testUtil.TimeoutUtils
 import io.kotlintest.shouldBe
@@ -23,11 +23,37 @@ class LanguageProviderTest {
         languageProvider = LanguageProvider()
     }
 
+    @Test(expected = IllegalArgumentException::class)
+    fun testProvideLanguage_localeMustNotBeNull() {
+        languageProvider.provideLanguage(null)
+    }
+
     @Test
-    @SdkSuppress(minSdkVersion = LOLLIPOP)
-    fun testProvideLanguage_shouldReturnCorrectLanguageCode() {
-        val result = languageProvider.provideLanguage(Locale.US)
+    fun testProvideLanguage_shouldReturnCorrectLanguageCode_whenOnlyLanguageUsed() {
+        val result = languageProvider.provideLanguage(Locale("en"))
+
+        result shouldBe "en"
+    }
+
+    @Test
+    fun testProvideLanguage_shouldReturnCorrectLanguageCode_whenLanguageAndCountryUsed() {
+        val result = languageProvider.provideLanguage(Locale("en", "US"))
 
         result shouldBe "en-US"
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = P)
+    fun testProvideLanguage_shouldReturnCorrectLanguageCode_whenLocaleBuilderUsed() {
+        val result = languageProvider.provideLanguage(Locale.Builder().setLanguage("zh").setScript("Hans").setRegion("CN").build())
+
+        result shouldBe "zh-Hans-CN"
+    }
+
+    @Test
+    fun testProvideLanguage_shouldReturnCorrectLanguageCode() {
+        val result = languageProvider.provideLanguage(Locale.SIMPLIFIED_CHINESE)
+
+        result shouldBe "zh-CN"
     }
 }
