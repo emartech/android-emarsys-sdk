ce2441359127d2c2837cb30202795d439b70cdc2
megamegax <mhunyady@emarsys.com>
2019-02-13 16:06:21 +0100

    feat(client-state-response-handler): add MobileEngageClientStateResponseHandler SUITEDEV-16337
    Co-authored-by: pjozsef <9980358+pjozsef@users.noreply.github.com>

---
+++ mobile-engage/src/androidTest/java/com/emarsys/mobileengage/responsehandler/MobileEngageClientStateResponseHandlerTest.kt
@@ -0,0 +1,87 @@
+package com.emarsys.mobileengage.responsehandler
+
+import com.emarsys.core.request.model.RequestModel
+import com.emarsys.core.response.ResponseModel
+import com.emarsys.mobileengage.endpoint.Endpoint
+import com.emarsys.mobileengage.storage.Storage
+import com.emarsys.testUtil.mockito.MockitoTestUtils.whenever
+import io.kotlintest.shouldBe
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import java.net.URL
+
+class MobileEngageClientStateResponseHandlerTest {
+
+    private companion object {
+        const val X_CLIENT_STATE_VALUE = "TG9yZW0gSXBzdW0gaXMgc2ltcGx5IGR1bW15IHRleHQgb2YgdGhlIHByaW50aW5nIGFuZCB0"
+    }
+
+    private lateinit var mockStorage: Storage<String>
+    private lateinit var requestModelMock: RequestModel
+    private lateinit var clientStateResponseHandler: MobileEngageClientStateResponseHandler
+
+    @Before
+    @Suppress("UNCHECKED_CAST")
+    fun setUp() {
+        mockStorage = Mockito.mock(Storage::class.java) as Storage<String>
+
+        requestModelMock = Mockito.mock(RequestModel::class.java).apply {
+            whenever(url).thenReturn(URL(Endpoint.ME_V3_CLIENT_BASE))
+        }
+        clientStateResponseHandler = MobileEngageClientStateResponseHandler(mockStorage)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testConstructor_clientStateStorage_mustNotBeNull() {
+        MobileEngageClientStateResponseHandler(null)
+    }
+
+    @Test
+    fun testShouldHandleResponse_false_whenUrl_isNotForMobileEngage() {
+        whenever(requestModelMock.url).thenReturn(URL("https://not-mobile-engage.com"))
+
+        val result = clientStateResponseHandler.shouldHandleResponse(responseModelWithClientState())
+
+        result shouldBe false
+    }
+
+    @Test
+    fun testShouldHandleResponse_false_whenClientStateIsNotPresent() {
+        val result = clientStateResponseHandler.shouldHandleResponse(responseModelWithoutClientState())
+
+        result shouldBe false
+    }
+
+    @Test
+    fun testShouldHandleResponse_true_whenRequestWasForMobileEngage_andContainsClientState() {
+        val result = clientStateResponseHandler.shouldHandleResponse(responseModelWithClientState())
+
+        result shouldBe true
+    }
+
+    @Test
+    fun testHandleResponse_storesClientState() {
+        clientStateResponseHandler.handleResponse(responseModelWithClientState())
+
+        verify(mockStorage).set(X_CLIENT_STATE_VALUE)
+    }
+
+    private fun responseModelWithClientState(): ResponseModel {
+        return ResponseModel.Builder()
+                .statusCode(200)
+                .message("OK")
+                .headers(mapOf("X-CLIENT-STATE" to listOf(X_CLIENT_STATE_VALUE)))
+                .requestModel(requestModelMock)
+                .build()
+    }
+
+    private fun responseModelWithoutClientState(): ResponseModel {
+        return ResponseModel.Builder()
+                .statusCode(200)
+                .message("OK")
+                .requestModel(requestModelMock)
+                .build()
+    }
+}
---
+++ mobile-engage/src/main/java/com/emarsys/mobileengage/responsehandler/MobileEngageClientStateResponseHandler.java
@@ -0,0 +1,37 @@
+package com.emarsys.mobileengage.responsehandler;
+
+import com.emarsys.core.response.AbstractResponseHandler;
+import com.emarsys.core.response.ResponseModel;
+import com.emarsys.core.util.Assert;
+import com.emarsys.mobileengage.storage.Storage;
+import com.emarsys.mobileengage.util.RequestUrlUtils;
+
+public class MobileEngageClientStateResponseHandler extends AbstractResponseHandler {
+
+    private static final String X_CLIENT_STATE = "X-CLIENT-STATE";
+
+    private final Storage<String> clientStateStorage;
+
+    public MobileEngageClientStateResponseHandler(Storage<String> clientStateStorage) {
+        Assert.notNull(clientStateStorage, "ClientStateStorage must not be null!");
+
+        this.clientStateStorage = clientStateStorage;
+    }
+
+    @Override
+    protected boolean shouldHandleResponse(ResponseModel responseModel) {
+        boolean isMobileEngageRequest = RequestUrlUtils.isMobileEngageRequest(responseModel.getRequestModel());
+        boolean hasClientState = getClientState(responseModel) != null;
+
+        return isMobileEngageRequest && hasClientState;
+    }
+
+    @Override
+    protected void handleResponse(ResponseModel responseModel) {
+        clientStateStorage.set(getClientState(responseModel));
+    }
+
+    private String getClientState(ResponseModel responseModel) {
+        return responseModel.getHeaders().get(X_CLIENT_STATE);
+    }
+}
