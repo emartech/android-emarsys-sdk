0e95681f970250a9e5fd17590d1ad109102b57c9
megamegax <mhunyady@emarsys.com>
2019-02-21 14:44:14 +0100

    feat(sample): use EmarsysSDKSampleSandbox App for MobileEngage as integration Application SUITEDEV-16247
    Co-authored-by: LaszloOriEmarsys <24588073+LaszloOriEmarsys@users.noreply.github.com>

--- sample/src/main/java/com/emarsys/sample/configuration/MobileEngageCredentials.java
+++ sample/src/main/java/com/emarsys/sample/configuration/MobileEngageCredentials.java
@@ -1,8 +1,8 @@
 package com.emarsys.sample.configuration;
 
 public class MobileEngageCredentials {
-    public static String INTEGRATION_APPLICATION_CODE = "14C19-A121F";
-    public static String INTEGRATION_APPLICATION_PASSWORD = "PaNkfOD90AVpYimMBuZopCpm8OWCrREu";
+    public static String INTEGRATION_APPLICATION_CODE = "EMS11-C3FD3";
+    public static String INTEGRATION_APPLICATION_PASSWORD = "kyuQy3bwWUfLLp7Gy4pjXnehRvArTWOs";
 
     public static String PROD_APPLICATION_CODE = "EMSA1-927A9";
     public static String PROD_APPLICATION_PASSWORD = "kQ8qXPzCuzzXZ9jTnuRT09zcv6aKsYf0";
