fee1d78e002b8bf5fdecfe4c9b8fc8b2647fae11
dschuppa <d.schuppa@gmail.com>
2019-07-11 09:20:26 +0200

    feat(sample-app): created bottom navigation SUITEDEV-18140
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: rpspace <4520949+rpspace@users.noreply.github.com>

--- new-sample-app/src/main/java/com/emarsys/newsampleapp/MainActivity.kt
+++ new-sample-app/src/main/java/com/emarsys/newsampleapp/MainActivity.kt
@@ -2,11 +2,38 @@ package com.emarsys.newsampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        setupBottomNavMenu(navController)
+        setupActionBar(navController)
     }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        bottom_navigation_view?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setupActionBar(navController: NavController) {
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val navigated = NavigationUI.onNavDestinationSelected(item, navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
+
 }
