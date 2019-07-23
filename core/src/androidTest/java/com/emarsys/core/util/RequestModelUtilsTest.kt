5822216bda9cffaee6bc54c0b461fc0bbe0e5db5
dschuppa <d.schuppa@gmail.com>
2019-07-23 10:10:52 +0200

    feat(request-model-utils): added method to extract query parameters SUITEDEV-18425
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- core/src/androidTest/java/com/emarsys/core/util/RequestModelUtilsTest.kt
+++ core/src/androidTest/java/com/emarsys/core/util/RequestModelUtilsTest.kt
@@ -36,17 +36,64 @@ class RequestModelUtilsTest {
 
     @Test
     fun testExtractIdsFromCompositeRequestModel_requestModel() {
-        val requestModel = RequestModel(
-                "https://emarsys.com",
-                RequestMethod.POST,
-                null,
-                emptyMap(),
-                100,
-                900000,
-                "requestModelId")
+        val requestModel = createTestRequestModel("https://emarsys.com", RequestMethod.POST)
 
         val result = RequestModelUtils.extractIdsFromCompositeRequestModel(requestModel)
 
         result shouldBe listOf("requestModelId")
     }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testExtractQueryParameters_requestModelMustNotBeNull() {
+        RequestModelUtils.extractQueryParameters(null)
+    }
+
+    @Test
+    fun testExtractQueryParameters_returnsEmptyMap_whenNoQueryParametersFound() {
+        val requestModel = createTestRequestModel(
+                "https://recommender.scarabresearch.com/merchants/", RequestMethod.GET)
+
+        val result = RequestModelUtils.extractQueryParameters(requestModel)
+        val expected = emptyMap<String, String>()
+
+        result shouldBe expected
+    }
+
+    @Test
+    fun testExtractExtractQueryParameters_resultMapContainsParameterFromQuery() {
+        val requestModel = createTestRequestModel(
+                "https://recommender.scarabresearch.com/merchants/merchantId?cp=1",
+                RequestMethod.GET)
+
+        val result = RequestModelUtils.extractQueryParameters(requestModel)
+        val expected = mapOf("cp" to "1")
+
+        result shouldBe expected
+    }
+
+    @Test
+    fun testExtractExtractQueryParameters_withMultipleQueryParameters() {
+        val requestModel = createTestRequestModel(
+                "https://recommender.scarabresearch.com/merchants/merchantId?cp=1&vi=888999888&ci=12345&q3=c",
+                RequestMethod.GET)
+
+        val result = RequestModelUtils.extractQueryParameters(requestModel)
+        val expected = mapOf(
+                "cp" to "1",
+                "vi" to "888999888",
+                "ci" to "12345",
+                "q3" to "c")
+
+        result shouldBe expected
+    }
+
+    private fun createTestRequestModel(url: String, method: RequestMethod) = RequestModel(
+            url,
+            method,
+            null,
+            emptyMap(),
+            100,
+            90000,
+            "requestModelId"
+    )
 }
