511929163c10f404ffffe0aa3ab0a343fb32b733
megamegax <mhunyady@emarsys.com>
2019-07-30 10:26:28 +0200

    test(logic): add timeoutRule SUITEDEV-18427
    Co-authored-by: Janos Szule <28537782+jszule@users.noreply.github.com>

--- predict-api/src/androidTest/java/com/emarsys/predict/api/model/RecommendationLogicTest.java
+++ predict-api/src/androidTest/java/com/emarsys/predict/api/model/RecommendationLogicTest.java
@@ -1,6 +1,10 @@
 package com.emarsys.predict.api.model;
 
+import com.emarsys.testUtil.TimeoutUtils;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +12,9 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 
 public class RecommendationLogicTest {
+
+    @Rule
+    public TestRule timeout = TimeoutUtils.getTimeoutRule();
 
     @Test
     public void testSearch_shouldFillFields() {
