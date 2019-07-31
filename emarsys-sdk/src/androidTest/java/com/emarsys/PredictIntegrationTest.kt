4257308c36dd9ad09aa572091686739ed21b972b
LasOri <laszlo.ori@emarsys.com>
2019-07-31 11:31:02 +0200

    test(recommendation_test): recommendation integration tests has been extended with popular logic SUITEDEV-18648
    Co-authored-by: Janos Szule <28537782+jszule@users.noreply.github.com>

--- emarsys-sdk/src/androidTest/java/com/emarsys/PredictIntegrationTest.kt
+++ emarsys-sdk/src/androidTest/java/com/emarsys/PredictIntegrationTest.kt
@@ -296,6 +296,16 @@ class PredictIntegrationTest {
     }
 
     @Test
+    fun testRecommendProducts_withPopular() {
+        Emarsys.Predict.recommendProducts(RecommendationLogic.popular("MEN>Shirts"),
+                eventuallyStoreResultInProperty(this::triedRecommendedProducts.setter)).eventuallyAssert {
+            triedRecommendedProducts.errorCause shouldBe null
+            triedRecommendedProducts.result shouldNotBe null
+            triedRecommendedProducts.result!!.size shouldBeGreaterThan 0
+        }
+    }
+
+    @Test
     fun testMultipleInvocations() {
         testTrackCart()
         latch = CountDownLatch(1)
