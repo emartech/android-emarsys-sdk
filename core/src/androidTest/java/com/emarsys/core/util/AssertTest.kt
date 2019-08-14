55edd76185a77220522d218e0bc3006120a7a39d
megamegax <mhunyady@emarsys.com>
2019-08-14 10:02:47 +0200

    test(assert): convert test to kotlin SUITEDEV-18684
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>

---
+++ core/src/androidTest/java/com/emarsys/core/util/AssertTest.kt
@@ -0,0 +1,201 @@
+package com.emarsys.core.util
+
+import com.emarsys.testUtil.TimeoutUtils
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import java.util.*
+
+class AssertTest {
+
+    @Rule
+    @JvmField
+    val timeout: TestRule = TimeoutUtils.timeoutRule
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testNotNull_shouldThrowException_whenArgumentIsNull() {
+        Assert.notNull(null, "")
+    }
+
+    @Test
+    fun testNotNull_shouldNotThrowException_whenArgumentIsNotNull() {
+        try {
+            Assert.notNull("", null)
+        } catch (e: Exception) {
+            fail("testNotNull should not throw expection when object is not null: " + e.message)
+        }
+    }
+
+    @Test
+    fun testNotNull_shouldThrowException_withMessage_whenArgumentIsNull() {
+        val message = "message"
+        try {
+            Assert.notNull(null, message)
+            fail("Should throw exception")
+        } catch (e: Exception) {
+            assertEquals(message, e.message)
+        }
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testElementsNotNull_array_shouldThrowException_whenArgumentIsNull() {
+        Assert.elementsNotNull(null as Array<Any>?, "")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testElementsNotNull_array_shouldThrowException_whenContainsNullElement() {
+        Assert.elementsNotNull(arrayOf(Any(), Any(), null, Any()), "")
+    }
+
+    @Test
+    fun testElementsNotNull_array_shouldThrowException_whenContainsNullElement_withSpecifiedMessage() {
+        val message = "message"
+        try {
+            Assert.elementsNotNull(arrayOf(Any(), Any(), null, Any()), message)
+            fail("Should throw exception")
+        } catch (iae: IllegalArgumentException) {
+            assertEquals(message, iae.message)
+        }
+    }
+
+    @Test
+    fun testElementsNotNull_array_shouldNotThrowException_whenArrayLacksNullElement() {
+        try {
+            Assert.elementsNotNull(arrayOf(Any(), Any(), "randomstring"), null)
+        } catch (e: Exception) {
+            fail("testElementsNotNull should not throw expection when object is not null: " + e.message)
+        }
+    }
+
+    @Test
+    fun testElementsNotNull_array_shouldNotThrowException_whenArray_isEmpty() {
+        try {
+            Assert.elementsNotNull(arrayOf(), null)
+        } catch (e: Exception) {
+            fail("testElementsNotNull should not throw expection when object is not null: " + e.message)
+        }
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testElementsNotNull_list_shouldThrowException_whenArgumentIsNull() {
+        Assert.elementsNotNull(null as List<*>?, "")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testElementsNotNull_list_shouldThrowException_whenContainsNullElement() {
+        Assert.elementsNotNull(listOf(Any(), Any(), null, Any()), "")
+    }
+
+    @Test
+    fun testElementsNotNull_list_shouldThrowException_whenContainsNullElement_withSpecifiedMessage() {
+        val message = "message"
+        try {
+            Assert.elementsNotNull(listOf(Any(), Any(), null, Any()), message)
+            fail("Should throw exception")
+        } catch (iae: IllegalArgumentException) {
+            assertEquals(message, iae.message)
+        }
+    }
+
+    @Test
+    fun testElementsNotNull_list_shouldNotThrowException_whenListLacksNullElement() {
+        try {
+            Assert.elementsNotNull(listOf(Any(), Any(), "randomstring"), null)
+        } catch (e: Exception) {
+            fail("testElementsNotNull should not throw expection when object is not null: " + e.message)
+        }
+    }
+
+    @Test
+    fun testElementsNotNull_list_shouldNotThrowException_whenList_isEmpty() {
+        try {
+            Assert.elementsNotNull(ArrayList<Any>(), null)
+        } catch (e: Exception) {
+            fail("testElementsNotNull should not throw expection when object is not null: " + e.message)
+        }
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testNotEmpty_array_shouldNotAcceptNull() {
+        Assert.notEmpty(null as Array<Any>?, "message")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testNotEmpty_array_shouldThrowException_whenArrayIsEmpty() {
+        Assert.notEmpty(arrayOf(), "")
+    }
+
+    @Test
+    fun testNotEmpty_array_shouldThrowException_whenArrayIsEmpty_withSpecifiedMessage() {
+        val message = "message"
+        try {
+            Assert.notEmpty(arrayOf(), message)
+            fail("Should throw exception")
+        } catch (iae: IllegalArgumentException) {
+            assertEquals(message, iae.message)
+        }
+    }
+
+    @Test
+    fun testNotEmpty_array_shouldThrowException_whenArrayIsEmpty_withDefaultMessage() {
+        try {
+            Assert.notEmpty(arrayOf(), null)
+            fail("Should throw exception")
+        } catch (iae: IllegalArgumentException) {
+            assertEquals("Argument must not be empty!", iae.message)
+        }
+    }
+
+    @Test
+    fun testNotEmpty_array_shouldNotThrowException_whenArrayHasItems() {
+        try {
+            Assert.notEmpty(arrayOf<Any>("a", 3, Math.PI), null)
+        } catch (e: Exception) {
+            fail("testElementsNotNull should not throw expection when object is not null: " + e.message)
+        }
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testNotEmpty_list_shouldNotAcceptNull() {
+        Assert.notEmpty(null as List<*>?, "message")
+    }
+
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testNotEmpty_list_shouldThrowException_whenArrayIsEmpty() {
+        Assert.notEmpty(listOf<Any>(), "")
+    }
+
+    @Test
+    fun testNotEmpty_list_shouldThrowException_whenArrayIsEmpty_withSpecifiedMessage() {
+        val message = "message"
+        try {
+            Assert.notEmpty(listOf<Any>(), message)
+            fail("Should throw exception")
+        } catch (iae: IllegalArgumentException) {
+            assertEquals(message, iae.message)
+        }
+    }
+
+    @Test
+    fun testNotEmpty_list_shouldThrowException_whenArrayIsEmpty_withDefaultMessage() {
+        try {
+            Assert.notEmpty(listOf<Any>(), null)
+            fail("Should throw exception")
+        } catch (iae: IllegalArgumentException) {
+            assertEquals("Argument must not be empty!", iae.message)
+        }
+    }
+
+    @Test
+    fun testNotEmpty_list_shouldNotThrowException_whenArrayHasItems() {
+        try {
+            Assert.notEmpty(listOf("a", 3, Math.PI), null)
+        } catch (e: Exception) {
+            fail("testElementsNotNull should not throw expection when object is not null: " + e.message)
+        }
+    }
+
+}
