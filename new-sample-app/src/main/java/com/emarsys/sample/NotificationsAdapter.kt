92c580c9b042a19a3541301ccd296b6cf5e8b2d4
dschuppa <d.schuppa@gmail.com>
2019-09-03 12:07:05 +0200

    feat(inbox) added ui and functionality for inbox SUITEDEV-18140
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

---
+++ new-sample-app/src/main/java/com/emarsys/sample/NotificationsAdapter.kt
@@ -0,0 +1,40 @@
+package com.emarsys.sample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.emarsys.mobileengage.api.inbox.Notification
+import kotlinx.android.synthetic.main.notification_view.view.*
+
+class NotificationsAdapter : RecyclerView.Adapter<NotificationsAdapter.NotificationViewHolder>() {
+    private var notifications = mutableListOf<Notification>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
+        return NotificationViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.notification_view, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return notifications.size
+    }
+
+    override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
+        holder.title.text = notifications[position].title
+        holder.body.text = notifications[position].body
+        holder.image.text = notifications[position].customData["image_url"] ?: "no image"
+    }
+
+    fun addItems(notifications: List<Notification>) {
+        this.notifications = notifications as MutableList<Notification>
+        notifyDataSetChanged()
+
+    }
+
+    class NotificationViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.notification_title
+        val body: TextView = view.notification_body
+        val image: TextView = view.notification_image
+    }
+
+}
