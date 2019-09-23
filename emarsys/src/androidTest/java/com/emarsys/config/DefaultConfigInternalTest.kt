121ab30b33e643a67557ef038cd2266918f1e29a
megamegax <mhunyady@emarsys.com>
2019-09-23 16:22:55 +0200

    feat(config): make applicationCode changeable SUITEDEV-19154
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>

---
+++ emarsys/src/androidTest/java/com/emarsys/config/DefaultConfigInternalTest.kt
@@ -0,0 +1,119 @@
+package com.emarsys.config
+
+import com.emarsys.core.api.result.CompletionListener
+import com.emarsys.core.storage.Storage
+import com.emarsys.mobileengage.MobileEngageInternal
+import com.emarsys.mobileengage.MobileEngageRequestContext
+import com.emarsys.mobileengage.push.PushInternal
+import com.emarsys.mobileengage.push.PushTokenProvider
+import com.emarsys.predict.PredictInternal
+import com.emarsys.predict.request.PredictRequestContext
+import com.emarsys.testUtil.mockito.whenever
+import io.kotlintest.shouldBe
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import java.util.concurrent.CountDownLatch
+
+class DefaultConfigInternalTest {
+    private companion object {
+        const val APPLICATION_CODE = "applicationCode"
+        const val OTHER_APPLICATION_CODE = "otherApplicationCode"
+        const val CONTACT_FIELD_ID = 3
+        const val CONTACT_FIELD_VALUE = "originalContactFieldValue"
+        const val PUSH_TOKEN = "pushToken"
+    }
+
+    private lateinit var configInternal: ConfigInternal
+    private lateinit var mockMobileEngageRequestContext: MobileEngageRequestContext
+    private lateinit var mockPredictRequestContext: PredictRequestContext
+    private lateinit var mockMobileEngageInternal: MobileEngageInternal
+    private lateinit var mockPushInternal: PushInternal
+    private lateinit var mockPushTokenProvider: PushTokenProvider
+    private lateinit var mockPredictInternal: PredictInternal
+    private lateinit var mockContactFieldValueStorage: Storage<String>
+
+    @Before
+    @Suppress("UNCHECKED_CAST")
+    fun setUp() {
+        mockPushTokenProvider = mock(PushTokenProvider::class.java)
+        whenever(mockPushTokenProvider.providePushToken()).thenReturn(PUSH_TOKEN)
+
+        mockContactFieldValueStorage = (mock(Storage::class.java) as Storage<String>).apply {
+            whenever(get()).thenReturn(CONTACT_FIELD_VALUE).thenReturn(null)
+        }
+
+        mockMobileEngageRequestContext = mock(MobileEngageRequestContext::class.java).apply {
+            whenever(applicationCode).thenReturn(APPLICATION_CODE)
+            whenever(contactFieldValueStorage).thenReturn(mockContactFieldValueStorage)
+        }
+        mockMobileEngageInternal = mock(MobileEngageInternal::class.java).apply {
+            whenever(clearContact(any())).thenAnswer { invocation ->
+                mockContactFieldValueStorage.get()
+                (invocation.getArgument(0) as CompletionListener).onCompleted(null)
+            }
+            whenever(setContact(any(), any())).thenAnswer { invocation ->
+                (invocation.getArgument(1) as CompletionListener).onCompleted(null)
+            }
+        }
+        mockPushInternal = mock(PushInternal::class.java).apply {
+            whenever(setPushToken(any(), any())).thenAnswer { invocation ->
+                (invocation.getArgument(1) as CompletionListener).onCompleted(null)
+            }
+        }
+
+        mockPredictInternal = mock(PredictInternal::class.java)
+        mockPredictRequestContext = mock(PredictRequestContext::class.java)
+
+        configInternal = DefaultConfigInternal(mockMobileEngageRequestContext, mockMobileEngageInternal, mockPushInternal, mockPushTokenProvider)
+    }
+
+    @Test
+    fun testGetContactFieldId_shouldReturnValueFromRequestContext() {
+        whenever(mockMobileEngageRequestContext.contactFieldId).thenReturn(CONTACT_FIELD_ID)
+
+        val result = configInternal.contactFieldId
+
+        result shouldBe CONTACT_FIELD_ID
+    }
+
+    @Test
+    fun testGetApplicationCode_shouldReturnValueFromRequestContext() {
+        whenever(mockMobileEngageRequestContext.applicationCode).thenReturn(APPLICATION_CODE)
+
+        val result = configInternal.applicationCode
+
+        result shouldBe APPLICATION_CODE
+    }
+
+    @Test
+    fun testChangeApplicationCode_shouldCallClearContact() {
+        configInternal.changeApplicationCode(OTHER_APPLICATION_CODE, CompletionListener { })
+
+        verify(mockMobileEngageInternal).clearContact(any(CompletionListener::class.java))
+    }
+
+    @Test
+    fun testChangeApplicationCode_shouldCallSetContactWithOriginalContactFieldIdAndContactFieldValue() {
+        configInternal.changeApplicationCode(OTHER_APPLICATION_CODE, CompletionListener {
+            verify(mockMobileEngageInternal).clearContact(any(CompletionListener::class.java))
+            verify(mockMobileEngageInternal).setContact(eq(CONTACT_FIELD_VALUE), any())
+        })
+    }
+
+    @Test
+    fun testChangeApplicationCode_shouldCallSetPushToken() {
+        val latch = CountDownLatch(1)
+        configInternal.changeApplicationCode(OTHER_APPLICATION_CODE, CompletionListener {
+            latch.countDown()
+        })
+        latch.await()
+
+        verify(mockMobileEngageInternal).clearContact(any(CompletionListener::class.java))
+        verify(mockMobileEngageInternal).setContact(eq(CONTACT_FIELD_VALUE), any())
+        verify(mockPushInternal).setPushToken(eq(PUSH_TOKEN), any())
+    }
+}
---
+++ emarsys/src/main/java/com/emarsys/config/DefaultConfigInternal.kt
@@ -0,0 +1,31 @@
+package com.emarsys.config
+
+import com.emarsys.core.api.result.CompletionListener
+import com.emarsys.mobileengage.MobileEngageInternal
+import com.emarsys.mobileengage.MobileEngageRequestContext
+import com.emarsys.mobileengage.push.PushInternal
+import com.emarsys.mobileengage.push.PushTokenProvider
+
+class DefaultConfigInternal(private val mobileEngageRequestContext: MobileEngageRequestContext,
+                            private val mobileEngageInternal: MobileEngageInternal,
+                            private val pushInternal: PushInternal,
+                            private val pushTokenProvider: PushTokenProvider) : ConfigInternal {
+    override val applicationCode: String
+        get() = mobileEngageRequestContext.applicationCode
+
+    override val contactFieldId: Int
+        get() = mobileEngageRequestContext.contactFieldId
+
+
+    override fun changeApplicationCode(applicationCode: String?, completionListener: CompletionListener?) {
+        val originalContactFieldValue = mobileEngageRequestContext.contactFieldValueStorage.get()
+        mobileEngageInternal.clearContact {
+            pushInternal.setPushToken(pushTokenProvider.providePushToken()) {
+                mobileEngageInternal.setContact(originalContactFieldValue, completionListener)
+            }
+        }
+    }
+
+    override fun changeMerchantId(merchantId: String?) {
+    }
+}
