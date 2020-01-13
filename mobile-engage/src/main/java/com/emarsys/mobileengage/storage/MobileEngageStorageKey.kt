b006d2cfd8543cbba277ca549f98a90f15e8a579
megamegax <mhunyady@emarsys.com>
2020-01-13 15:49:41 +0100

    chore(mobile-engage): convert MobileEngageStorageKey to Kotlin SUITEDEV-19922
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

---
+++ mobile-engage/src/main/java/com/emarsys/mobileengage/storage/MobileEngageStorageKey.kt
@@ -0,0 +1,12 @@
+package com.emarsys.mobileengage.storage
+
+import com.emarsys.core.storage.StorageKey
+import java.util.*
+
+enum class MobileEngageStorageKey : StorageKey {
+    REFRESH_TOKEN, CONTACT_TOKEN, CLIENT_STATE, CONTACT_FIELD_VALUE, PUSH_TOKEN, EVENT_SERVICE_URL, CLIENT_SERVICE_URL, INBOX_SERVICE_URL, DEEPLINK_SERVICE_URL, ME_V2_SERVICE_URL;
+
+    override fun getKey(): String {
+        return "mobile_engage_" + name.toLowerCase(Locale.getDefault())
+    }
+}
