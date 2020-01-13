283b65760d269677c5bc53fdc1a2279643c5637f
megamegax <mhunyady@emarsys.com>
2020-01-13 15:50:40 +0100

    feat(predict): eliminate warning SUITEDEV-19922
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

--- predict/src/main/java/com/emarsys/predict/storage/PredictStorageKey.kt
+++ predict/src/main/java/com/emarsys/predict/storage/PredictStorageKey.kt
@@ -1,11 +1,12 @@
 package com.emarsys.predict.storage
 
 import com.emarsys.core.storage.StorageKey
+import java.util.*
 
 enum class PredictStorageKey : StorageKey {
     PREDICT_SERVICE_URL;
 
     override fun getKey(): String {
-        return "predict_" + name.toLowerCase()
+        return "predict_" + name.toLowerCase(Locale.getDefault())
     }
 }
