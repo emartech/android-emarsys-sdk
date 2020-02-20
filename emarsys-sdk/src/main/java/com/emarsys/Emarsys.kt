d51be464ae248c3c81cc406be778ee17038c7a18
zkovacs <zsolt.kovacs@emarsys.com>
2020-02-20 16:13:39 +0100

    docs(emarsys): depracate old methods SUITEDEV-20937
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- emarsys-sdk/src/main/java/com/emarsys/Emarsys.kt
+++ emarsys-sdk/src/main/java/com/emarsys/Emarsys.kt
@@ -263,10 +263,12 @@ object Emarsys {
     object Push {
 
         @JvmStatic
+        @Deprecated(message = "This method is not neccesary anymore. EmarsysMessagingService takes care of it.", replaceWith = ReplaceWith("EmarsysMessagingService.handleMessage()"))
         fun trackMessageOpen(intent: Intent) {
             push.trackMessageOpen(intent)
         }
 
+        @Deprecated(message = "This method is not neccesary anymore. EmarsysMessagingService takes care of it.", replaceWith = ReplaceWith("EmarsysMessagingService.handleMessage()"))
         @JvmStatic
         fun trackMessageOpen(
                 intent: Intent,
@@ -274,6 +276,7 @@ object Emarsys {
             trackMessageOpen(intent, CompletionListener { completionListener(it) })
         }
 
+        @Deprecated(message = "This method is not neccesary anymore. EmarsysMessagingService takes care of it.", replaceWith = ReplaceWith("EmarsysMessagingService.handleMessage()"))
         @JvmStatic
         fun trackMessageOpen(
                 intent: Intent,
