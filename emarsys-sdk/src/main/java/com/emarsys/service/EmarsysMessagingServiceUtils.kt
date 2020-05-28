942c33ce4e403cbddf8234d3297e6e5189e5a370
LasOri <laszlo.oeri@gmail.com>
2020-05-28 15:51:19 +0200

    fix(messaging-service): fix proxy SUITEDEV-22634
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com> 
    Co-authored-by: megamegax <megamegax@users.noreply.github.com> 
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

--- emarsys-sdk/src/main/java/com/emarsys/service/EmarsysMessagingServiceUtils.kt
+++ emarsys-sdk/src/main/java/com/emarsys/service/EmarsysMessagingServiceUtils.kt
@@ -1,6 +1,7 @@
 package com.emarsys.service
 
 import android.content.Context
+import android.os.Handler
 import com.emarsys.core.api.proxyApi
 import com.emarsys.core.di.getDependency
 import com.emarsys.core.provider.timestamp.TimestampProvider
@@ -8,6 +9,7 @@ import com.emarsys.core.util.Assert
 import com.emarsys.mobileengage.service.MessagingServiceUtils
 import com.emarsys.mobileengage.service.MessagingServiceUtils.handleMessage
 import com.google.firebase.messaging.RemoteMessage
+import java.util.concurrent.CountDownLatch
 
 object EmarsysMessagingServiceUtils {
     var MESSAGE_FILTER = MessagingServiceUtils.MESSAGE_FILTER
@@ -16,16 +18,25 @@ object EmarsysMessagingServiceUtils {
     fun handleMessage(context: Context, remoteMessage: RemoteMessage): Boolean {
         Assert.notNull(context, "Context must not be null!")
         Assert.notNull(remoteMessage, "RemoteMessage must not be null!")
-        return handleMessage(
-                context,
-                remoteMessage,
-                getDependency(),
-                getDependency(),
-                getDependency<TimestampProvider>(),
-                getDependency(),
-                getDependency("silentMessageActionCommandFactory"))
-                .proxyApi(getDependency("coreSdkHandler"))
 
+        val latch = CountDownLatch(1)
+        val handler: Handler = getDependency("coreSdkHandler")
+        var result = false
+
+        handler.post {
+            result = handleMessage(
+                    context,
+                    remoteMessage,
+                    getDependency(),
+                    getDependency(),
+                    getDependency<TimestampProvider>(),
+                    getDependency(),
+                    getDependency("silentMessageActionCommandFactory"))
+            latch.countDown()
+        }
+        latch.await()
+
+        return result
     }
 
     fun isMobileEngageMessage(remoteMessageData: Map<String, String?>?): Boolean {
