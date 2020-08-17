403499fa490da6f35d3280c4a70985f2853b8cb7
megamegax <megamegax@gmail.com>
2020-08-17 15:03:44 +0200

    fix(iam-dialog): fix the specified child already has a parent crash SUITEDEV-24010
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

---
+++ mobile-engage/src/androidTest/java/com/emarsys/mobileengage/iam/dialog/IamDialogTest.kt
@@ -0,0 +1,372 @@
+package com.emarsys.mobileengage.iam.dialog
+
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.os.Build.VERSION_CODES
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.webkit.WebView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.emarsys.core.di.DependencyInjection
+import com.emarsys.core.provider.timestamp.TimestampProvider
+import com.emarsys.core.util.log.entry.InAppLoadingTime
+import com.emarsys.mobileengage.fake.FakeMobileEngageDependencyContainer
+import com.emarsys.mobileengage.iam.dialog.IamDialog.Companion.create
+import com.emarsys.mobileengage.iam.dialog.action.OnDialogShownAction
+import com.emarsys.mobileengage.iam.webview.IamWebViewProvider
+import com.emarsys.testUtil.InstrumentationRegistry.Companion.getTargetContext
+import com.emarsys.testUtil.ReflectionTestUtils.setStaticField
+import com.emarsys.testUtil.TimeoutUtils.timeoutRule
+import com.emarsys.testUtil.fake.FakeActivity
+import com.nhaarman.mockitokotlin2.*
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import java.util.concurrent.CountDownLatch
+
+@SdkSuppress(minSdkVersion = VERSION_CODES.KITKAT)
+class IamDialogTest {
+    private companion object {
+        const val CAMPAIGN_ID = "id_value"
+        private const val SID = "testSid"
+        private const val URL = "https://www.emarsys.com"
+        const val ON_SCREEN_TIME_KEY = "on_screen_time"
+        const val CAMPAIGN_ID_KEY = "id"
+        const val REQUEST_ID_KEY = "request_id"
+    }
+
+    private lateinit var dialog: TestIamDialog
+    private lateinit var mockTimestampProvider: TimestampProvider
+
+    @Rule
+    @JvmField
+    var timeout: TestRule = timeoutRule
+
+    @Rule
+    @JvmField
+    var activityRule = ActivityTestRule(FakeActivity::class.java)
+
+    @Before
+    @Throws(InterruptedException::class)
+    fun setUp() {
+        mockTimestampProvider = mock()
+        DependencyInjection.setup(FakeMobileEngageDependencyContainer(timestampProvider = mockTimestampProvider))
+        dialog = spy(TestIamDialog.create(
+                CAMPAIGN_ID,
+                CountDownLatch(1),
+                CountDownLatch(1),
+                CountDownLatch(1),
+                CountDownLatch(1)
+        ))
+        dialog.setActions(listOf())
+        dialog.setInAppLoadingTime(InAppLoadingTime(1, 1))
+        initWebViewProvider()
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun tearDown() {
+        DependencyInjection.tearDown()
+        setWebViewInProvider(null)
+    }
+
+    @Test
+    fun testCreate_shouldReturnImageDialogInstance() {
+        create("", SID, URL, "requestId") shouldNotBe null
+    }
+
+    @Test
+    fun testCreate_shouldInitializeDialog_withCampaignId() {
+        val campaignId = "123456789"
+        val dialog = create(campaignId, SID, URL, "requestId")
+        val result = dialog.arguments
+
+        result shouldNotBe null
+        result!!.getString(CAMPAIGN_ID_KEY) shouldBe campaignId
+    }
+
+    @Test
+    fun testCreate_shouldInitializeDialog_withRequestId() {
+        val requestId = "requestId"
+        val campaignId = "campaignId"
+        val dialog = create(campaignId, SID, URL, requestId)
+        val result = dialog.arguments
+
+        result shouldNotBe null
+        result!!.getString(REQUEST_ID_KEY) shouldBe requestId
+    }
+
+    @Test
+    fun testCreate_shouldInitializeDialog_withSid() {
+        val requestId = "requestId"
+        val campaignId = "campaignId"
+        val dialog = create(campaignId, SID, URL, requestId)
+        val result = dialog.arguments
+
+        result shouldNotBe null
+        result!!.getString("sid") shouldBe "testSid"
+    }
+
+    @Test
+    fun testCreate_shouldInitializeDialog_withUrl() {
+        val requestId = "requestId"
+        val campaignId = "campaignId"
+        val dialog = create(campaignId, SID, URL, requestId)
+        val result = dialog.arguments
+
+        result shouldNotBe null
+        result!!.getString("url") shouldBe "https://www.emarsys.com"
+    }
+
+    @Test
+    fun testCreate_shouldInitializeDialog_withOutRequestId() {
+        val campaignId = "campaignId"
+        val dialog = create(campaignId, SID, URL, null)
+        val result = dialog.arguments
+
+        result shouldNotBe null
+        result!!.getString(REQUEST_ID_KEY) shouldBe null
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testInitialization_setsDimAmountToZero() {
+        displayDialog()
+        val expected = 0.0f
+        val actual = dialog.dialog!!.window!!.attributes.dimAmount
+        org.junit.Assert.assertEquals(expected.toDouble(), actual.toDouble(), 0.0000001)
+
+
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testInitialization_setsDialogToFullscreen() {
+        displayDialog()
+        val dialogWidth = activityRule.activity.window.attributes.width.toFloat()
+        val dialogHeight = activityRule.activity.window.attributes.height.toFloat()
+        val windowWidth = dialog.dialog!!.window!!.attributes.width.toFloat()
+        val windowHeight = dialog.dialog!!.window!!.attributes.height.toFloat()
+        org.junit.Assert.assertEquals(windowWidth.toDouble(), dialogWidth.toDouble(), 0.0001)
+        org.junit.Assert.assertEquals(windowHeight.toDouble(), dialogHeight.toDouble(), 0.0001)
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testDialog_stillVisible_afterOrientationChange() {
+        val iamDialog = create(CAMPAIGN_ID, SID, URL, REQUEST_ID_KEY)
+        val activity: AppCompatActivity = activityRule.activity
+        activity.runOnUiThread { iamDialog.show(activity.supportFragmentManager, "testDialog") }
+        activityRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+        initWebViewProvider()
+        activityRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+        initWebViewProvider()
+        val dialogWidth = activityRule.activity.window.attributes.width.toFloat()
+        val dialogHeight = activityRule.activity.window.attributes.height.toFloat()
+        val windowWidth = iamDialog.dialog!!.window!!.attributes.width.toFloat()
+        val windowHeight = iamDialog.dialog!!.window!!.attributes.height.toFloat()
+        org.junit.Assert.assertEquals(windowWidth.toDouble(), dialogWidth.toDouble(), 0.0001)
+        org.junit.Assert.assertEquals(windowHeight.toDouble(), dialogHeight.toDouble(), 0.0001)
+    }
+
+    @Test
+    fun testDialog_cancel_turnsRetainInstanceOff() {
+        val iamDialog = create(CAMPAIGN_ID, SID, URL, REQUEST_ID_KEY)
+        iamDialog.setInAppLoadingTime(InAppLoadingTime(1, 1))
+        val activity: AppCompatActivity = activityRule.activity
+        activity.runOnUiThread { iamDialog.show(activity.supportFragmentManager, "testDialog") }
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+        iamDialog.dialog!!.cancel()
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+
+        iamDialog.retainInstance shouldBe false
+    }
+
+    @Test
+    fun testDialog_dismiss_turnsRetainInstanceOff() {
+        val iamDialog = create(CAMPAIGN_ID, SID, URL, REQUEST_ID_KEY)
+        iamDialog.setInAppLoadingTime(InAppLoadingTime(1, 1))
+        val activity: AppCompatActivity = activityRule.activity
+        activity.runOnUiThread { iamDialog.show(activity.supportFragmentManager, "testDialog") }
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+        iamDialog.dismiss()
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+
+        iamDialog.retainInstance shouldBe false
+    }
+
+    @Test
+    fun testOnResume_callsActions_ifProvided() {
+        val args = Bundle()
+        args.putString(CAMPAIGN_ID_KEY, "123456789")
+        dialog.arguments = args
+        val actions = createMockActions()
+        dialog.setActions(actions)
+        displayDialog()
+        for (action in actions) {
+            verify(action).execute("123456789", null, null)
+        }
+    }
+
+    @Test
+    fun testOnResume_callsActions_onlyOnce() {
+        val actions = createMockActions()
+        dialog.setActions(actions)
+        displayDialog()
+        dismissDialog()
+        displayDialog()
+        for (action in actions) {
+            verify(action, times(1)).execute(any(), any(), any())
+        }
+    }
+
+    @Test
+    fun testOnScreenTime_savesDuration_betweenResumeAndPause() {
+        whenever(mockTimestampProvider.provideTimestamp()).thenReturn(100L, 250L)
+
+        displayDialog()
+        pauseDialog()
+        val onScreenTime = dialog.arguments!!.getLong(ON_SCREEN_TIME_KEY)
+
+        dialog.arguments shouldNotBe null
+        onScreenTime shouldBe 150
+    }
+
+    @Test
+    fun testOnScreenTime_aggregatesDurations_betweenMultipleResumeAndPause() {
+        whenever(mockTimestampProvider.provideTimestamp()).thenReturn(100L, 250L, 1000L, 1003L)
+
+
+        displayDialog()
+        pauseDialog()
+        dialog.arguments shouldNotBe null
+        dialog.arguments!!.getLong(ON_SCREEN_TIME_KEY) shouldBe 150
+        resumeDialog()
+        pauseDialog()
+        dialog.arguments shouldNotBe null
+        dialog.arguments!!.getLong(ON_SCREEN_TIME_KEY) shouldBe 153
+    }
+
+    @Test
+    fun testOnStart_shouldNotThrowTheSpecifiedChildAlreadyHasAParent_exception() {
+        var result: Exception? = null
+        try {
+            displayDialog()
+            val activity: AppCompatActivity = activityRule.activity
+            activity.runOnUiThread { dialog.onStart() }
+        } catch (exception: IllegalStateException) {
+            result = exception
+        }
+
+        result shouldBe null
+    }
+
+    @Test
+    fun testOnStart_shouldNotThrowTheSpecifiedWebViewAlreadyHasAParent_exception() {
+        var result: Exception? = null
+        try {
+            initWebViewProvider()
+            Handler(Looper.getMainLooper()).post {
+                val webView = IamWebViewProvider(getTargetContext()).provideWebView()
+                LinearLayout(getTargetContext()).addView(webView)
+            }
+            displayDialog()
+            val activity: AppCompatActivity = activityRule.activity
+            activity.runOnUiThread { dialog.onStart() }
+        } catch (exception: IllegalStateException) {
+            result = exception
+        }
+        result shouldBe null
+    }
+
+    @Test
+    fun testOnStart_shouldNotThrowCannotAddANullChildViewToAViewGroup_exception() {
+        setStaticField(IamWebViewProvider::class.java, "webView", null)
+        var result: Exception? = null
+        try {
+            displayDialog()
+        } catch (exception: IllegalArgumentException) {
+            result = exception
+        }
+        result shouldBe null
+        verify(dialog).dismiss()
+    }
+
+    private fun displayDialog() {
+        dialog.resumeLatch = CountDownLatch(1)
+        val activity: AppCompatActivity = activityRule.activity
+        activity.runOnUiThread { dialog.show(activity.supportFragmentManager, "testDialog") }
+        dialog.resumeLatch.await()
+    }
+
+    private fun resumeDialog() {
+        dialog.resumeLatch = CountDownLatch(1)
+        val activity: Activity = activityRule.activity
+        activity.runOnUiThread { dialog.onResume() }
+        dialog.resumeLatch.await()
+    }
+
+    private fun pauseDialog() {
+        dialog.pauseLatch = CountDownLatch(1)
+        val activity: Activity = activityRule.activity
+        activity.runOnUiThread { dialog.onPause() }
+        dialog.pauseLatch.await()
+    }
+
+    private fun cancelDialog() {
+        dialog.cancelLatch = CountDownLatch(1)
+        val activity: Activity = activityRule.activity
+        activity.runOnUiThread { dialog.onCancel(mock()) }
+        dialog.cancelLatch.await()
+    }
+
+    private fun dismissDialog() {
+        dialog.stopLatch = CountDownLatch(1)
+        val activity: Activity = activityRule.activity
+        activity.runOnUiThread { dialog.dismiss() }
+        dialog.stopLatch.await()
+    }
+
+    @Throws(Exception::class)
+    private fun setWebViewInProvider(webView: WebView?) {
+        val webViewField = IamWebViewProvider::class.java.getDeclaredField("webView")
+        webViewField.isAccessible = true
+        webViewField[null] = webView
+    }
+
+    private fun initWebViewProvider() {
+        val initLatch = CountDownLatch(1)
+        Handler(Looper.getMainLooper()).post {
+            try {
+                setWebViewInProvider(WebView(getTargetContext()))
+            } catch (e: Exception) {
+                throw RuntimeException(e)
+            }
+            initLatch.countDown()
+        }
+        initLatch.await()
+    }
+
+    private fun createMockActions(): List<OnDialogShownAction> {
+        return listOf(
+                mock(),
+                mock(),
+                mock()
+        )
+    }
+}
---
+++ mobile-engage/src/main/java/com/emarsys/mobileengage/iam/dialog/IamDialog.kt
@@ -0,0 +1,187 @@
+package com.emarsys.mobileengage.iam.dialog
+
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.webkit.WebView
+import android.widget.FrameLayout
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.DialogFragment
+import com.emarsys.core.di.getDependency
+import com.emarsys.core.provider.timestamp.TimestampProvider
+import com.emarsys.core.util.log.Logger.Companion.info
+import com.emarsys.core.util.log.entry.AppEventLog
+import com.emarsys.core.util.log.entry.InAppLoadingTime
+import com.emarsys.core.util.log.entry.InAppLog
+import com.emarsys.core.util.log.entry.OnScreenTime
+import com.emarsys.mobileengage.R
+import com.emarsys.mobileengage.iam.dialog.action.OnDialogShownAction
+import com.emarsys.mobileengage.iam.webview.IamWebViewProvider
+
+@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+open class IamDialog : DialogFragment() {
+
+    companion object {
+        const val TAG = "MOBILE_ENGAGE_IAM_DIALOG_TAG"
+        const val CAMPAIGN_ID = "id"
+        const val SID = "sid"
+        const val URL = "url"
+        const val REQUEST_ID = "request_id"
+        const val IS_SHOWN = "isShown"
+        const val ON_SCREEN_TIME = "on_screen_time"
+        const val END_SCREEN_TIME = "end_screen_time"
+        const val LOADING_TIME = "loading_time"
+
+        @JvmStatic
+        fun create(campaignId: String, sid: String?, url: String?, requestId: String?): IamDialog {
+            val iamDialog = IamDialog()
+            val bundle = Bundle()
+            bundle.putString(CAMPAIGN_ID, campaignId)
+            bundle.putString(SID, sid)
+            bundle.putString(URL, url)
+            bundle.putString(REQUEST_ID, requestId)
+            iamDialog.arguments = bundle
+            return iamDialog
+        }
+    }
+
+    private var actions: List<OnDialogShownAction>? = null
+    private lateinit var webViewContainer: FrameLayout
+    private var webView: WebView? = null
+    private var startTime: Long = 0
+    private var dismissed = false
+    val timestampProvider: TimestampProvider by lazy { getDependency<TimestampProvider>() }
+
+    fun setActions(actions: List<OnDialogShownAction>) {
+        this.actions = actions
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+        setStyle(STYLE_NO_FRAME, android.R.style.Theme_Dialog)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.mobile_engage_in_app_message, container, false)
+        webView = IamWebViewProvider(activity!!.applicationContext).provideWebView()
+        webViewContainer = v.findViewById(R.id.mobileEngageInAppMessageContainer)
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+        webViewContainer.removeAllViews()
+        if (webView != null) {
+            if (webView!!.parent == null) {
+                webViewContainer.addView(webView)
+            }
+
+            val window = dialog?.window
+
+            dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            val windowParams = window?.attributes
+            windowParams?.dimAmount = 0.0f
+            window?.attributes = windowParams
+            dialog?.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT,
+                    WindowManager.LayoutParams.MATCH_PARENT)
+        } else {
+            dismiss()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startTime = timestampProvider.provideTimestamp()
+        val args = arguments
+        if (args != null) {
+            val notShown = !args.getBoolean(IS_SHOWN, false)
+            if (notShown) {
+                actions?.forEach { action ->
+                    val campaignId = args.getString(CAMPAIGN_ID)
+                    val sid = args.getString(SID)
+                    val url = args.getString(URL)
+                    action.execute(campaignId, sid, url)
+                    args.putBoolean(IS_SHOWN, true)
+                }
+            }
+        }
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        saveOnScreenTime()
+        retainInstance = false
+        super.onCancel(dialog)
+    }
+
+    override fun dismiss() {
+        saveOnScreenTime()
+        retainInstance = false
+        super.dismiss()
+    }
+
+    override fun onPause() {
+        updateOnScreenTime()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        webViewContainer.removeView(webView)
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (webView != null) {
+            webView!!.removeAllViews()
+            webView!!.destroy()
+        }
+    }
+
+    override fun onDestroyView() {
+        if (dialog != null && retainInstance) {
+            dialog!!.setDismissMessage(null)
+        }
+        super.onDestroyView()
+    }
+
+    fun setInAppLoadingTime(inAppLoadingTime: InAppLoadingTime?) {
+        arguments?.putParcelable(LOADING_TIME, inAppLoadingTime)
+    }
+
+    private fun updateOnScreenTime() {
+        if (!dismissed) {
+            val endScreenTime = timestampProvider.provideTimestamp()
+            val currentDuration = endScreenTime - startTime
+            val previousDuration = arguments!!.getLong(ON_SCREEN_TIME)
+            arguments?.putLong(ON_SCREEN_TIME, previousDuration + currentDuration)
+            arguments?.putLong(END_SCREEN_TIME, endScreenTime)
+        }
+    }
+
+    private fun saveOnScreenTime() {
+        updateOnScreenTime()
+        val args = arguments
+        if (args != null) {
+            info(InAppLog(
+                    args.getParcelable(LOADING_TIME)!!,
+                    OnScreenTime(
+                            args.getLong(ON_SCREEN_TIME),
+                            startTime,
+                            args.getLong(END_SCREEN_TIME)),
+                    args.getString(CAMPAIGN_ID)!!,
+                    args.getString(REQUEST_ID)))
+        } else {
+            error(AppEventLog("reporting iamDialog", mapOf("error" to "iamDialog - arguments has been null")))
+        }
+        dismissed = true
+    }
+}
