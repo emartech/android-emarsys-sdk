603a99ade619356136d50e3ffee6797f52d78c1f
megamegax <mhunyady@emarsys.com>
2020-08-31 11:46:04 +0200

    feat(inline-inapp): expand sample app with examples SUITEDEV-23428
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

--- sample/src/main/java/com/emarsys/sample/adapters/MessageInboxAdapter.kt
+++ sample/src/main/java/com/emarsys/sample/adapters/MessageInboxAdapter.kt
@@ -1,5 +1,4 @@
 package com.emarsys.sample.adapters
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,29 +10,26 @@ import com.emarsys.mobileengage.api.inbox.Message
 import com.emarsys.sample.R
 import com.emarsys.sample.TagChangeListener
 import kotlinx.android.synthetic.main.notification_view_with_labels.view.*
-
-
 class MessageInboxAdapter(private val tagChangeListener: TagChangeListener) : RecyclerView.Adapter<MessageInboxAdapter.NotificationViewHolder>() {
     private var notifications = mutableListOf<Message>()
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
         return NotificationViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.notification_view_with_labels, parent, false))
     }
-
     override fun getItemCount(): Int {
         return notifications.size
     }
-
     override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
         holder.title.text = notifications[position].title
         holder.body.text = notifications[position].body
-
+        val tags = notifications[position].tags
+        if (!tags.isNullOrEmpty()){
+            holder.messageLabels.text = tags.toString()
+        }
         val imageUrl = notifications[position].imageUrl
         holder.image.load(imageUrl){
             placeholder(R.drawable.placeholder)
             crossfade(true)
         }
-
         holder.addButton.setOnClickListener {
             tagChangeListener.addTagClicked(notifications[position].id)
         }
@@ -41,20 +37,16 @@ class MessageInboxAdapter(private val tagChangeListener: TagChangeListener) : Re
             tagChangeListener.removeTagClicked(notifications[position].id)
         }
     }
-
     fun addItems(notifications: List<Message>) {
         this.notifications = notifications as MutableList<Message>
         notifyDataSetChanged()
-
     }
-
     class NotificationViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = view.notification_title
         val body: TextView = view.notification_body
         val image: ImageView = view.notification_image
         val addButton = view.add_label_button
         val removeButton = view.remove_label_button
+        val messageLabels: TextView = view.message_labels
     }
-
-
 }
---
+++ sample/src/main/java/com/emarsys/sample/fragments/InlineInAppFragment.kt
@@ -0,0 +1,55 @@
+package com.emarsys.sample.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.emarsys.core.api.result.CompletionListener
+import com.emarsys.inapp.ui.InlineInAppView
+import com.emarsys.sample.R
+import com.emarsys.sample.extensions.showSnackBar
+import kotlinx.android.synthetic.main.fragment_inline_in_app.*
+
+class InlineInAppFragment : Fragment() {
+    companion object {
+        val TAG: String = InlineInAppFragment::class.java.simpleName
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_inline_in_app, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        inlineInAppFullyFromXml.onCloseListener = {
+            inlineInAppFullyFromXml.visibility = View.GONE
+        }
+
+        inlineInAppFromXmlAndCode.loadInApp("ia")
+        inlineInAppFromXmlAndCode.onCompletionListener = CompletionListener {
+            Log.d(TAG, "Inline in-App has been showed")
+        }
+        inlineInAppFromXmlAndCode.onCloseListener = {
+            inlineInAppFromXmlAndCode.visibility = View.GONE
+        }
+
+        inlineInAppFromXmlAndCode.onAppEventListener = { property, json ->
+            view.showSnackBar("AppEvent - $property, $json")
+        }
+
+        showInlineInApp.setOnClickListener {
+            val inlineInApp = InlineInAppView(it.context)
+            inlineInApp.onCloseListener = {
+                inlineInApp.visibility = View.GONE
+            }
+            inlineInAppContainer.addView(inlineInApp)
+            inlineInAppContainer.invalidate()
+
+            inlineInApp.loadInApp(viewId.text.toString())
+        }
+    }
+}
