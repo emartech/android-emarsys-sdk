a5c334278ded29a6188969f98bbb6601343034f8
megamegax <mhunyady@emarsys.com>
2021-01-20 10:05:04 +0100

    chore(dependencies): upgrade kotlin and gradle plugins SUITEDEV-26225
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val kotlin_version = "1.4.10"
+    const val kotlin_version = "1.4.21"
     const val kotlin_test_version = "3.1.11"
     const val mockito_version = "2.25.0"
     const val espresso_version = "3.3.0"
