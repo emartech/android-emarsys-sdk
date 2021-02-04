c5e64a15b85229a73535e82c7a081e7f7290c0db
dschuppa <d.schuppa@gmail.com>
2021-02-04 10:14:38 +0100

    feat(event-service-v4): implement InAppCleanUpResponseHandlerV4 SUITEDEV-26531
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

---
+++ mobile-engage/src/androidTest/java/com/emarsys/mobileengage/responsehandler/InAppCleanUpResponseHandlerV4Test.kt
@@ -0,0 +1,185 @@
+package com.emarsys.mobileengage.responsehandler
+
+import com.emarsys.common.feature.InnerFeature
+import com.emarsys.core.database.repository.Repository
+import com.emarsys.core.database.repository.SqlSpecification
+import com.emarsys.core.endpoint.ServiceEndpointProvider
+import com.emarsys.core.feature.FeatureRegistry
+import com.emarsys.core.request.model.RequestModel
+import com.emarsys.core.response.ResponseModel
+import com.emarsys.mobileengage.iam.model.buttonclicked.ButtonClicked
+import com.emarsys.mobileengage.iam.model.displayediam.DisplayedIam
+import com.emarsys.mobileengage.iam.model.specification.FilterByCampaignId
+import com.emarsys.testUtil.FeatureTestUtils
+import com.emarsys.testUtil.TimeoutUtils
+import com.nhaarman.mockitokotlin2.*
+import io.kotlintest.data.forall
+import io.kotlintest.shouldBe
+import io.kotlintest.tables.row
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import java.net.URL
+
+class InAppCleanUpResponseHandlerV4Test {
+    companion object {
+        private const val EVENT_HOST = "https://mobile-events.eservice.emarsys.net"
+        private const val EVENT_BASE = "$EVENT_HOST/v4/apps/%s/events"
+    }
+
+    private lateinit var handler: InAppCleanUpResponseHandlerV4
+    private lateinit var mockDisplayedIamRepository: Repository<DisplayedIam, SqlSpecification>
+    private lateinit var mockButtonClickRepository: Repository<ButtonClicked, SqlSpecification>
+    private lateinit var mockEventServiceProvider: ServiceEndpointProvider
+    private lateinit var mockEventServiceV4Provider: ServiceEndpointProvider
+    private lateinit var mockRequestModel: RequestModel
+
+    @Rule
+    @JvmField
+    val timeout: TestRule = TimeoutUtils.timeoutRule
+
+    @Before
+    fun setUp() {
+        mockRequestModel = mock {
+            on { url } doReturn URL(EVENT_BASE)
+            on { payload } doReturn mapOf(
+                    "clicks" to listOf(
+                            mapOf("campaignId" to "123", "buttonId" to "123", "timestamp" to "1234"),
+                            mapOf("campaignId" to "456", "buttonId" to "456", "timestamp" to "5678")),
+                    "viewedMessages" to listOf(
+                            mapOf("campaignId" to "78910", "buttonId" to "123123", "timestamp" to "1233214"),
+                            mapOf("campaignId" to "6543", "buttonId" to "234", "timestamp" to "45321")
+                    )
+            )
+        }
+        mockDisplayedIamRepository = mock()
+        mockButtonClickRepository = mock()
+        mockEventServiceProvider = mock {
+            on { provideEndpointHost() } doReturn EVENT_HOST
+        }
+        mockEventServiceV4Provider = mock {
+            on { provideEndpointHost() } doReturn EVENT_HOST
+        }
+
+        handler = InAppCleanUpResponseHandlerV4(mockDisplayedIamRepository, mockButtonClickRepository, mockEventServiceProvider, mockEventServiceV4Provider)
+
+        FeatureRegistry.enableFeature(InnerFeature.EVENT_SERVICE_V4)
+    }
+
+    @After
+    fun tearDown() {
+        FeatureTestUtils.resetFeatures()
+    }
+
+    @Test
+    fun testShouldHandleResponse_shouldReturnFalse_whenNotMobileEngageEvent() {
+        whenever(mockRequestModel.url).thenReturn(URL("https://not-mobile-engage.com"))
+        val response = buildResponseModel(mockRequestModel)
+
+        val result = handler.shouldHandleResponse(response)
+
+        result shouldBe false
+    }
+
+    @Test
+    fun testShouldHandleResponse_shouldReturnFalse_whenEventServiceV4_isDisabled() {
+        FeatureRegistry.disableFeature(InnerFeature.EVENT_SERVICE_V4)
+
+        val response = buildResponseModel(mockRequestModel)
+
+        val result = handler.shouldHandleResponse(response)
+
+        result shouldBe false
+    }
+
+    @Test
+    fun testShouldHandleResponse_whenResponseWasSuccessful() {
+        forall(
+                row(buildResponseModel(mockRequestModel, statusCode = 200), true),
+                row(buildResponseModel(mockRequestModel, statusCode = 299), true),
+                row(buildResponseModel(mockRequestModel, statusCode = 400), false)
+        ) { input, expected ->
+            val result = handler.shouldHandleResponse(input)
+            result shouldBe expected
+        }
+    }
+
+    @Test
+    fun testShouldHandleResponse_shouldReturnFalse_whenRequestIsNullOrEmpty() {
+        whenever(mockRequestModel.payload).thenReturn(emptyMap())
+        val responseModel = buildResponseModel(mockRequestModel)
+
+        val result = handler.shouldHandleResponse(responseModel)
+
+        result shouldBe false
+    }
+
+    @Test
+    fun testShouldHandleResponse_shouldReturnTrue_whenRequestContainsOnlyClicksOrViewedMessages() {
+        whenever(mockRequestModel.payload).thenReturn(mapOf(
+                "clicks" to listOf(
+                        mapOf("campaignId" to "123", "buttonId" to "123", "timestamp" to "1234"),
+                        mapOf("campaignId" to "456", "buttonId" to "456", "timestamp" to "5678")),
+        ))
+
+        val responseModel = buildResponseModel(mockRequestModel)
+
+        val result = handler.shouldHandleResponse(responseModel)
+
+        result shouldBe true
+    }
+
+    @Test
+    fun testShouldHandleResponse_shouldReturnFalse_whenRequestContainsABody_butNotViewedMessagesOrClicks() {
+        whenever(mockRequestModel.payload).thenReturn(mapOf("key" to "value"))
+
+        val responseModel = buildResponseModel(mockRequestModel)
+
+        val result = handler.shouldHandleResponse(responseModel)
+
+        result shouldBe false
+    }
+
+    @Test
+    fun testHandleResponse_shouldRemoveSelectedCampaignIds_fromButtonClickedRepository() {
+        val responseModel = buildResponseModel(mockRequestModel)
+
+        handler.handleResponse(responseModel)
+
+        verify(mockButtonClickRepository).remove(FilterByCampaignId("123", "456"))
+    }
+
+    @Test
+    fun testHandleResponse_shouldRemoveSelectedCampaignIds_fromDisplayedMessagesRepository() {
+        val responseModel = buildResponseModel(mockRequestModel)
+
+        handler.handleResponse(responseModel)
+
+        verify(mockDisplayedIamRepository).remove(FilterByCampaignId("78910", "6543"))
+    }
+
+    @Test
+    fun testHandleResponse_shouldNotCallRepository_whenClicksOrViewedMessagesAreEmpty() {
+        whenever(mockRequestModel.payload).thenReturn(mapOf(
+                "clicks" to listOf<Map<String,Any?>>(),
+                "viewedMessages" to listOf()
+        ))
+        val responseModel = buildResponseModel(mockRequestModel)
+
+        handler.handleResponse(responseModel)
+
+        verifyZeroInteractions(mockButtonClickRepository)
+        verifyZeroInteractions(mockDisplayedIamRepository)
+    }
+
+    private fun buildResponseModel(requestModel: RequestModel, responseBody: String = "{'oldCampaigns': ['123', '456', '78910','6543']}", statusCode: Int = 200): ResponseModel {
+        return ResponseModel.Builder()
+                .statusCode(statusCode)
+                .message("OK")
+                .body(responseBody)
+                .requestModel(requestModel)
+                .build()
+    }
+}
---
+++ mobile-engage/src/main/java/com/emarsys/mobileengage/responsehandler/InAppCleanUpResponseHandlerV4.kt
@@ -0,0 +1,57 @@
+package com.emarsys.mobileengage.responsehandler
+
+import com.emarsys.common.feature.InnerFeature
+import com.emarsys.core.database.repository.Repository
+import com.emarsys.core.database.repository.SqlSpecification
+import com.emarsys.core.endpoint.ServiceEndpointProvider
+import com.emarsys.core.feature.FeatureRegistry
+import com.emarsys.core.request.model.RequestModel
+import com.emarsys.core.response.AbstractResponseHandler
+import com.emarsys.core.response.ResponseModel
+import com.emarsys.mobileengage.iam.model.buttonclicked.ButtonClicked
+import com.emarsys.mobileengage.iam.model.displayediam.DisplayedIam
+import com.emarsys.mobileengage.iam.model.specification.FilterByCampaignId
+import com.emarsys.mobileengage.util.RequestModelUtils
+
+class InAppCleanUpResponseHandlerV4(
+        private val displayedIamRepository: Repository<DisplayedIam, SqlSpecification>,
+        private val buttonClickedRepository: Repository<ButtonClicked, SqlSpecification>,
+        private val eventServiceProvider: ServiceEndpointProvider,
+        private val eventServiceV4Provider: ServiceEndpointProvider) : AbstractResponseHandler() {
+
+    override fun shouldHandleResponse(responseModel: ResponseModel): Boolean {
+        val requestModel = responseModel.requestModel
+        return FeatureRegistry.isFeatureEnabled(InnerFeature.EVENT_SERVICE_V4) &&
+                RequestModelUtils.isCustomEvent(requestModel, eventServiceProvider, eventServiceV4Provider) &&
+                responseModel.statusCode in 200..299 &&
+                (hasObject(requestModel, "viewedMessages") ||
+                        hasObject(requestModel, "clicks"))
+    }
+
+    override fun handleResponse(responseModel: ResponseModel) {
+        val payload = responseModel.requestModel.payload
+        if (payload != null && payload.containsKey("clicks")) {
+            val campaignIdsToRemove: Array<String> = collectCampaignIds(payload["clicks"] as List<Map<String, Any?>>)
+            if (campaignIdsToRemove.isNotEmpty()) {
+                buttonClickedRepository.remove(FilterByCampaignId(*campaignIdsToRemove))
+            }
+        }
+        if (payload != null && payload.containsKey("viewedMessages")) {
+            val campaignIdsToRemove: Array<String> = collectCampaignIds(payload["viewedMessages"] as List<Map<String, Any?>>)
+            if (campaignIdsToRemove.isNotEmpty()) {
+                displayedIamRepository.remove(FilterByCampaignId(*campaignIdsToRemove))
+            }
+        }
+    }
+
+    private fun collectCampaignIds(items: List<Map<String, Any?>>): Array<String> {
+        return items.map { buttonClick ->
+            buttonClick["campaignId"] as String
+        }.toTypedArray()
+    }
+
+    private fun hasObject(requestModel: RequestModel, key: String): Boolean {
+        val payload = requestModel.payload
+        return !payload.isNullOrEmpty() && payload.containsKey(key)
+    }
+}
