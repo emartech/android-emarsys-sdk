9544428f2a864679668d723ce3aa3349abe1f00c
Mihály Hunyady <mihaly.hunyady@emarsys.com>
2021-02-11 13:22:55 +0100

    chore(release): build before local release SUITEDEV-26225
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

--- local-release.sh
+++ local-release.sh
@@ -3,4 +3,4 @@ export EXCLUDE_GOOGLE_SERVICES_API_KEY=true
 echo Releasing with version: $1
 git tag -a $1 -m $1
 git push --tags
-./gradlew clean build -x lint -x test release
+./gradlew clean build lint -x test release
