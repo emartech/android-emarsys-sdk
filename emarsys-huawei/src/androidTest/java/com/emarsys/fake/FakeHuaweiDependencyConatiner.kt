17c96dbb260b3ec9d8c8892fcebd806d93c2f927
zkovacs <zsolt.kovacs@emarsys.com>
2021-06-24 14:54:05 +0200

    test(huawei): test EmarsysHuaweiMessagingService.kt SUITEDEV-28147
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

---
+++ emarsys-huawei/src/androidTest/java/com/emarsys/fake/FakeHuaweiDependencyConatiner.kt
@@ -0,0 +1,161 @@
+package com.emarsys.fake
+
+import android.content.SharedPreferences
+import android.os.Handler
+import android.os.Looper
+import com.emarsys.core.CoreCompletionHandler
+import com.emarsys.core.activity.ActivityLifecycleWatchdog
+import com.emarsys.core.activity.CurrentActivityWatchdog
+import com.emarsys.core.app.AppLifecycleObserver
+import com.emarsys.core.concurrency.CoreSdkHandlerProvider
+import com.emarsys.core.connection.ConnectionWatchDog
+import com.emarsys.core.crypto.Crypto
+import com.emarsys.core.database.CoreSQLiteDatabase
+import com.emarsys.core.database.helper.CoreDbHelper
+import com.emarsys.core.database.repository.Repository
+import com.emarsys.core.database.repository.SqlSpecification
+import com.emarsys.core.device.DeviceInfo
+import com.emarsys.core.endpoint.ServiceEndpointProvider
+import com.emarsys.core.handler.CoreSdkHandler
+import com.emarsys.core.provider.activity.CurrentActivityProvider
+import com.emarsys.core.provider.hardwareid.HardwareIdProvider
+import com.emarsys.core.provider.timestamp.TimestampProvider
+import com.emarsys.core.provider.uuid.UUIDProvider
+import com.emarsys.core.request.RequestManager
+import com.emarsys.core.request.RestClient
+import com.emarsys.core.request.model.RequestModel
+import com.emarsys.core.response.ResponseHandlersProcessor
+import com.emarsys.core.shard.ShardModel
+import com.emarsys.core.storage.KeyValueStore
+import com.emarsys.core.storage.Storage
+import com.emarsys.core.util.FileDownloader
+import com.emarsys.core.util.log.Logger
+import com.emarsys.core.worker.Worker
+import com.emarsys.mobileengage.MobileEngageInternal
+import com.emarsys.mobileengage.MobileEngageRequestContext
+import com.emarsys.mobileengage.RefreshTokenInternal
+import com.emarsys.mobileengage.client.ClientServiceInternal
+import com.emarsys.mobileengage.deeplink.DeepLinkInternal
+import com.emarsys.mobileengage.di.MobileEngageComponent
+import com.emarsys.mobileengage.event.EventHandlerProvider
+import com.emarsys.mobileengage.event.EventServiceInternal
+import com.emarsys.mobileengage.geofence.GeofenceInternal
+import com.emarsys.mobileengage.iam.InAppEventHandlerInternal
+import com.emarsys.mobileengage.iam.InAppInternal
+import com.emarsys.mobileengage.iam.OverlayInAppPresenter
+import com.emarsys.mobileengage.iam.inline.InlineInAppWebViewFactory
+import com.emarsys.mobileengage.iam.jsbridge.IamJsBridgeFactory
+import com.emarsys.mobileengage.iam.model.buttonclicked.ButtonClicked
+import com.emarsys.mobileengage.iam.model.displayediam.DisplayedIam
+import com.emarsys.mobileengage.iam.webview.WebViewProvider
+import com.emarsys.mobileengage.inbox.InboxInternal
+import com.emarsys.mobileengage.inbox.MessageInboxInternal
+import com.emarsys.mobileengage.inbox.model.NotificationCache
+import com.emarsys.mobileengage.notification.ActionCommandFactory
+import com.emarsys.mobileengage.push.NotificationInformationListenerProvider
+import com.emarsys.mobileengage.push.PushInternal
+import com.emarsys.mobileengage.push.PushTokenProvider
+import com.emarsys.mobileengage.push.SilentNotificationInformationListenerProvider
+import com.emarsys.mobileengage.request.CoreCompletionHandlerRefreshTokenProxyProvider
+import com.emarsys.mobileengage.request.MobileEngageRequestModelFactory
+import com.emarsys.mobileengage.responsehandler.MobileEngageTokenResponseHandler
+import com.emarsys.mobileengage.service.RemoteMessageMapper
+import com.emarsys.mobileengage.session.MobileEngageSession
+import com.emarsys.mobileengage.session.SessionIdHolder
+import com.emarsys.mobileengage.util.RequestModelHelper
+import com.nhaarman.mockitokotlin2.mock
+
+class FakeHuaweiDependencyConatiner(
+        override val coreSdkHandler: CoreSdkHandler = CoreSdkHandlerProvider().provideHandler(),
+        override val uiHandler: Handler = Handler(Looper.getMainLooper()),
+        override val mobileEngageInternal: MobileEngageInternal = mock(),
+        override val loggingMobileEngageInternal: MobileEngageInternal = mock(),
+        override val clientServiceInternal: ClientServiceInternal = mock(),
+        override val loggingClientServiceInternal: ClientServiceInternal = mock(),
+        override val inboxInternal: InboxInternal = mock(),
+        override val loggingInboxInternal: InboxInternal = mock(),
+        override val messageInboxInternal: MessageInboxInternal = mock(),
+        override val loggingMessageInboxInternal: MessageInboxInternal = mock(),
+        override val inAppInternal: InAppInternal = mock(),
+        override val loggingInAppInternal: InAppInternal = mock(),
+        override val deepLinkInternal: DeepLinkInternal = mock(),
+        override val loggingDeepLinkInternal: DeepLinkInternal = mock(),
+        override val pushInternal: PushInternal = mock(),
+        override val loggingPushInternal: PushInternal = mock(),
+        override val eventServiceInternal: EventServiceInternal = mock(),
+        override val loggingEventServiceInternal: EventServiceInternal = mock(),
+        override val refreshTokenInternal: RefreshTokenInternal = mock(),
+        override val inAppEventHandlerInternal: InAppEventHandlerInternal = mock(),
+        override val requestContext: MobileEngageRequestContext = mock(),
+        override val overlayInAppPresenter: OverlayInAppPresenter = mock(),
+        override val deviceInfoPayloadStorage: Storage<String?> = mock(),
+        override val contactFieldValueStorage: Storage<String?> = mock(),
+        override val contactTokenStorage: Storage<String?> = mock(),
+        override val clientStateStorage: Storage<String?> = mock(),
+        override val pushTokenStorage: Storage<String?> = mock(),
+        override val refreshTokenStorage: Storage<String?> = mock(),
+        override val clientServiceStorage: Storage<String?> = mock(),
+        override val eventServiceStorage: Storage<String?> = mock(),
+        override val deepLinkServiceStorage: Storage<String?> = mock(),
+        override val inboxServiceStorage: Storage<String?> = mock(),
+        override val messageInboxServiceStorage: Storage<String?> = mock(),
+        override val mobileEngageV2ServiceStorage: Storage<String?> = mock(),
+        override val deviceEventStateStorage: Storage<String?> = mock(),
+        override val responseHandlersProcessor: ResponseHandlersProcessor = mock(),
+        override val notificationCache: NotificationCache = mock(),
+        override val pushTokenProvider: PushTokenProvider = mock(),
+        override val clientServiceEndpointProvider: ServiceEndpointProvider = mock(),
+        override val eventServiceEndpointProvider: ServiceEndpointProvider = mock(),
+        override val deepLinkServiceProvider: ServiceEndpointProvider = mock(),
+        override val inboxServiceProvider: ServiceEndpointProvider = mock(),
+        override val messageInboxServiceProvider: ServiceEndpointProvider = mock(),
+        override val mobileEngageV2ServiceProvider: ServiceEndpointProvider = mock(),
+        override val notificationInformationListenerProvider: NotificationInformationListenerProvider = mock(),
+        override val silentNotificationInformationListenerProvider: SilentNotificationInformationListenerProvider = mock(),
+        override val notificationActionCommandFactory: ActionCommandFactory = mock(),
+        override val silentMessageActionCommandFactory: ActionCommandFactory = mock(),
+        override val notificationEventHandlerProvider: EventHandlerProvider = mock(),
+        override val silentMessageEventHandlerProvider: EventHandlerProvider = mock(),
+        override val onEventActionEventHandlerProvider: EventHandlerProvider = mock(),
+        override val geofenceEventHandlerProvider: EventHandlerProvider = mock(),
+        override val currentActivityProvider: CurrentActivityProvider = mock(),
+        override val geofenceInternal: GeofenceInternal = mock(),
+        override val loggingGeofenceInternal: GeofenceInternal = mock(),
+        override val buttonClickedRepository: Repository<ButtonClicked, SqlSpecification> = mock(),
+        override val displayedIamRepository: Repository<DisplayedIam, SqlSpecification> = mock(),
+        override val contactTokenResponseHandler: MobileEngageTokenResponseHandler = mock(),
+        override val webViewProvider: WebViewProvider = mock(),
+        override val inlineInAppWebViewFactory: InlineInAppWebViewFactory = mock(),
+        override val iamJsBridgeFactory: IamJsBridgeFactory = mock(),
+        override val remoteMessageMapper: RemoteMessageMapper = mock(),
+        override val appLifecycleObserver: AppLifecycleObserver = mock(),
+        override val requestModelHelper: RequestModelHelper = mock(),
+        override val sessionIdHolder: SessionIdHolder = mock(),
+        override val coreCompletionHandlerRefreshTokenProxyProvider: CoreCompletionHandlerRefreshTokenProxyProvider = mock(),
+        override val mobileEngageRequestModelFactory: MobileEngageRequestModelFactory = mock(),
+        override val mobileEngageSession: MobileEngageSession = mock(),
+        override val activityLifecycleWatchdog: ActivityLifecycleWatchdog = mock(),
+        override val currentActivityWatchdog: CurrentActivityWatchdog = mock(),
+        override val coreSQLiteDatabase: CoreSQLiteDatabase = mock(),
+        override val deviceInfo: DeviceInfo = mock(),
+        override val shardRepository: Repository<ShardModel, SqlSpecification> = mock(),
+        override val timestampProvider: TimestampProvider = mock(),
+        override val uuidProvider: UUIDProvider = mock(),
+        override val logShardTrigger: Runnable = mock(),
+        override val logger: Logger = mock(),
+        override val restClient: RestClient = mock(),
+        override val fileDownloader: FileDownloader = mock(),
+        override val keyValueStore: KeyValueStore = mock(),
+        override val sharedPreferences: SharedPreferences = mock(),
+        override val hardwareIdProvider: HardwareIdProvider = mock(),
+        override val coreDbHelper: CoreDbHelper = mock(),
+        override val hardwareIdStorage: Storage<String?> = mock(),
+        override val logLevelStorage: Storage<String?> = mock(),
+        override val crypto: Crypto = mock(),
+        override val requestManager: RequestManager = mock(),
+        override val worker: Worker = mock(),
+        override val requestModelRepository: Repository<RequestModel, SqlSpecification> = mock(),
+        override val connectionWatchdog: ConnectionWatchDog = mock(),
+        override val coreCompletionHandler: CoreCompletionHandler = mock(),
+        override val geofenceInitialEnterTriggerEnabledStorage: Storage<Boolean?> = mock()
+) : MobileEngageComponent
---
+++ emarsys-huawei/src/androidTest/java/com/emarsys/service/EmarsysHuaweiMessagingServiceTest.kt
@@ -0,0 +1,109 @@
+package com.emarsys.service
+
+import android.app.Application
+import android.os.Looper
+import com.emarsys.core.device.DeviceInfo
+import com.emarsys.core.handler.CoreSdkHandler
+import com.emarsys.fake.FakeHuaweiDependencyConatiner
+import com.emarsys.mobileengage.di.setupMobileEngageComponent
+import com.emarsys.mobileengage.di.tearDownMobileEngageComponent
+import com.emarsys.mobileengage.push.PushInternal
+import com.emarsys.testUtil.FeatureTestUtils
+import com.emarsys.testUtil.InstrumentationRegistry
+import com.emarsys.testUtil.TimeoutUtils
+import com.nhaarman.mockitokotlin2.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.stubbing.Answer
+import java.util.concurrent.CountDownLatch
+
+class EmarsysHuaweiMessagingServiceTest {
+
+    @Rule
+    @JvmField
+    val timeout: TestRule = TimeoutUtils.timeoutRule
+
+    private val application: Application
+        get() = InstrumentationRegistry.getTargetContext().applicationContext as Application
+
+    private lateinit var mockPushInternal: PushInternal
+    private lateinit var fakeDependencyContainer: FakeHuaweiDependencyConatiner
+    private lateinit var mockCoreSdkHandler: CoreSdkHandler
+    private lateinit var emarsysHuaweiMessagingService: EmarsysHuaweiMessagingService
+    val latch = CountDownLatch(1)
+
+
+    @Before
+    fun setUp() {
+        mockPushInternal = mock()
+
+        mockCoreSdkHandler = mock {
+            on { post(any()) } doAnswer Answer<Any?> { invocation ->
+                invocation.getArgument<Runnable>(0).run()
+                null
+            }
+        }
+        Looper.prepare()
+        emarsysHuaweiMessagingService = EmarsysHuaweiMessagingService()
+    }
+
+    @After
+    fun tearDown() {
+        tearDownMobileEngageComponent()
+        FeatureTestUtils.resetFeatures()
+    }
+
+    @Test
+    fun testOnNewToken_whenIsAutomaticPushSendingEnabledIsTrue_callsSetPushToken() {
+        setupEmarsys(true)
+
+        emarsysHuaweiMessagingService.onNewToken("testToken")
+
+        verify(mockPushInternal, timeout(100)).setPushToken("testToken", null)
+    }
+
+    @Test
+    fun testOnNewToken_whenIsAutomaticPushSendingEnabledIsTrue_callsSetPushToken_onCoreSdkThread() {
+        setupEmarsys(true)
+        emarsysHuaweiMessagingService.onNewToken("testToken")
+
+        verify(mockCoreSdkHandler, timeout(1000).times(1)).post(any())
+    }
+
+    @Test
+    fun testOnNewToken_whenIsAutomaticPushSendingEnabledIsFalse_doesNotCallSetPushToken() {
+        setupEmarsys(false)
+
+        emarsysHuaweiMessagingService.onNewToken("testToken")
+
+        verify(mockPushInternal, times(0)).setPushToken("testToken", null)
+    }
+
+    private fun setupEmarsys(isAutomaticPushSending: Boolean) {
+        val deviceInfo = DeviceInfo(
+                application,
+                mock {
+                    on { provideHardwareId() } doReturn "hardwareId"
+                },
+                mock {
+                    on { provideSdkVersion() } doReturn "version"
+                },
+                mock {
+                    on { provideLanguage(any()) } doReturn "language"
+                },
+                mock(),
+                isAutomaticPushSending
+        )
+
+        fakeDependencyContainer = FakeHuaweiDependencyConatiner(
+                coreSdkHandler = mockCoreSdkHandler,
+                deviceInfo = deviceInfo,
+                pushInternal = mockPushInternal
+        )
+
+        setupMobileEngageComponent(fakeDependencyContainer)
+    }
+}
