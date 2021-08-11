346ea24e932cd4b7950d3d6d902f5fc039e25d29
zkovacs <zsolt.kovacs@emarsys.com>
2021-08-11 09:34:42 +0200

    feat(config-builder): removed deprecation from EmarsysConfig.Builder SUITEDEV-28718
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- emarsys-sdk/src/main/java/com/emarsys/config/EmarsysConfig.kt
+++ emarsys-sdk/src/main/java/com/emarsys/config/EmarsysConfig.kt
@@ -12,7 +12,6 @@ data class EmarsysConfig(val application: Application,
                                      val sharedSecret: String? = null,
                                      val verboseConsoleLoggingEnabled: Boolean = false) {
 
-    @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
     class Builder {
         private lateinit var application: Application
         private var applicationCode: String? = null
@@ -23,7 +22,6 @@ data class EmarsysConfig(val application: Application,
         private var sharedPackageNames: List<String>? = null
         private var verboseConsoleLoggingEnabled: Boolean = false
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun from(baseConfig: EmarsysConfig): Builder {
             application = baseConfig.application
             applicationCode = baseConfig.applicationCode
@@ -36,55 +34,46 @@ data class EmarsysConfig(val application: Application,
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun application(application: Application): Builder {
             this.application = application
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun applicationCode(mobileEngageApplicationCode: String?): Builder {
             this.applicationCode = mobileEngageApplicationCode
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun merchantId(predictMerchantId: String?): Builder {
             this.merchantId = predictMerchantId
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun enableExperimentalFeatures(vararg experimentalFeatures: FlipperFeature): Builder {
             this.experimentalFeatures = listOf(*experimentalFeatures)
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun disableAutomaticPushTokenSending(): Builder {
             automaticPushTokenSending = false
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun sharedSecret(sharedSecret: String): Builder {
             this.sharedSecret = sharedSecret
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun sharedPackageNames(sharedPackageNames: List<String>): Builder {
             this.sharedPackageNames = sharedPackageNames
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun enableVerboseConsoleLogging(): Builder {
             this.verboseConsoleLoggingEnabled = true
             return this
         }
 
-        @Deprecated(message = "Deprecated in Kotlin, please use the EmarsysConfig property based constructor.")
         fun build(): EmarsysConfig {
             experimentalFeatures = if (experimentalFeatures == null) emptyList() else experimentalFeatures
             return EmarsysConfig(
