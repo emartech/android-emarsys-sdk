42b221d18a61ac2bed87fd652251960257eddb9e
dschuppa <d.schuppa@gmail.com>
2021-09-07 11:24:17 +0200

    fix(geofences): changed pending intent to be mutable SUITEDEV-28602
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>
    Co-authored-by: Benjamin Gehl <gehl.benjamin@gmail.com>

--- mobile-engage/src/main/java/com/emarsys/mobileengage/geofence/GeofencePendingIntentProvider.kt
+++ mobile-engage/src/main/java/com/emarsys/mobileengage/geofence/GeofencePendingIntentProvider.kt
@@ -10,6 +10,6 @@ class GeofencePendingIntentProvider(private val context: Context) {
     fun providePendingIntent(): PendingIntent {
         val intent = Intent("com.emarsys.sdk.GEOFENCE_ACTION")
 
-        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     }
 }
