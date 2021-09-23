f32b6ec2c1b113dabf0258bdf81587a6993a1756
davidSchuppa <d.schuppa@gmail.com>
2021-09-23 10:04:10 +0200

    chore(security): modified project name SUITEDEV-29033
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- blackduck.sh
+++ blackduck.sh
@@ -6,8 +6,8 @@ chmod +x detect.sh
 --detect.blackduck.signature.scanner.memory=4096 \
 --detect.blackduck.signature.scanner.paths=~/work/android-emarsys-sdk/android-emarsys-sdk \
 --detect.code.location.name="https://github.com/emartech/android-emarsys-sdk" \
---detect.project.name="Github:emartech / android-emarsys-sdk" \
---detect.project.version.name=android-emarsys-sdk \
+--detect.project.name="Github:emartech\ /\ android-emarsys-sdk" \
+--detect.project.version.name=master \
 --detect.report.timeout=4800 \
 --detect.source.path=~/work/android-emarsys-sdk/android-emarsys-sdk  \
 --detect.latest.release.version=6.7.0 \
