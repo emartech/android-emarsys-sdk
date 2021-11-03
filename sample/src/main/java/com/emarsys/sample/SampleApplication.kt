a8d42560b1725ad6a56f1642dca4b7c56b15054e
Hunyady Mihály <mihaly.hunyady@emarsys.com>
2021-11-03 14:42:16 +0100

    feat(shared-hardware-id): use the Example app for Shared HardwareId use-case SUITEDEV-29463
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- sample/src/main/java/com/emarsys/sample/SampleApplication.kt
+++ sample/src/main/java/com/emarsys/sample/SampleApplication.kt
@@ -28,10 +28,12 @@ open class SampleApplication : Application(), EventHandler, NotificationInformat
         Kotpref.init(this)
 
         val config = EmarsysConfig(
-            application = this,
-            applicationCode = getApplicationCode(),
-            merchantId = Cache.merchantId,
-            verboseConsoleLoggingEnabled = true
+                application = this,
+                applicationCode = getApplicationCode(),
+                merchantId = Cache.merchantId,
+                verboseConsoleLoggingEnabled = true,
+                sharedPackageNames = listOf("com.emarsys.emarsys_sdk_example"),
+                sharedSecret = "secret"
         )
 
         createNotificationChannels()
@@ -55,29 +57,29 @@ open class SampleApplication : Application(), EventHandler, NotificationInformat
     private fun createNotificationChannels() {
         if (Build.VERSION.SDK_INT >= 26) {
             createNotificationChannel(
-                "ems_sample_news",
-                "News",
-                "News and updates go into this channel",
-                NotificationManager.IMPORTANCE_HIGH
+                    "ems_sample_news",
+                    "News",
+                    "News and updates go into this channel",
+                    NotificationManager.IMPORTANCE_HIGH
             )
             createNotificationChannel(
-                "ems_sample_messages",
-                "Messages",
-                "Important messages go into this channel",
-                NotificationManager.IMPORTANCE_HIGH
+                    "ems_sample_messages",
+                    "Messages",
+                    "Important messages go into this channel",
+                    NotificationManager.IMPORTANCE_HIGH
             )
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private fun createNotificationChannel(
-        id: String,
-        name: String,
-        description: String,
-        importance: Int
+            id: String,
+            name: String,
+            description: String,
+            importance: Int
     ) {
         val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val channel = NotificationChannel(id, name, importance)
         channel.description = description
         notificationManager.createNotificationChannel(channel)
