ed5ffacb0736d49d882ea9c8f6b25ce1402f4a45
Andras Sarro <andras.sarro@emarsys.com>
2022-03-11 15:02:11 +0100

    feat(async-proxy): async proxy uses coroutines and awaits result of non-void methods SUITEDEV-30461
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- core/src/main/java/com/emarsys/core/api/AsyncProxy.kt
+++ core/src/main/java/com/emarsys/core/api/AsyncProxy.kt
@@ -1,56 +1,70 @@
 package com.emarsys.core.api
 
 import com.emarsys.core.handler.ConcurrentHandlerHolder
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 
 inline fun <reified T : Any> T.proxyWithHandler(
     handlerHolder: ConcurrentHandlerHolder,
-    timeout: Long = 10
 ): T {
     return Proxy.newProxyInstance(
         javaClass.classLoader,
         javaClass.interfaces,
-        AsyncProxy(this, handlerHolder, timeout)
+        AsyncProxy(this, handlerHolder)
     ) as T
 }
 
 class AsyncProxy<T>(
     private val apiObject: T,
     private val handlerHolder: ConcurrentHandlerHolder,
-    private val timeout: Long
 ) : InvocationHandler {
 
     override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any? {
         EmarsysIdlingResources.increment()
-        var result: Any? = null
+
         val isVoid = method.returnType == Void.TYPE
-        if (method.returnType.isPrimitive) {
-            result = when (method.returnType) {
-                Boolean::class.java -> false
-                Char::class.java -> Char(0)
-                else -> 0
-            }
-        }
-        val latch = CountDownLatch(1)
-        handlerHolder.coreHandler.post {
-            result = if (args != null) {
-                method.invoke(apiObject, *args)
+
+        val result =
+            if (isOnCoreSdkThread()) {
+                invokeMethod(method, args)
             } else {
-                method.invoke(apiObject)
+                runBlocking(handlerHolder.sdkScope.coroutineContext) {
+                    schedule(isVoid) {
+                        invokeMethod(method, args)
+                    }
+                }
+            }
+
+        EmarsysIdlingResources.decrement()
+        return result
+
+    }
+
+    private fun isOnCoreSdkThread() =
+        Thread.currentThread() == handlerHolder.coreHandler.handler.looper.thread
+
+    private fun invokeMethod(method: Method, args: Array<out Any>?): Any? {
+        return if (args != null) {
+            method.invoke(apiObject, *args)
+        } else {
+            method.invoke(apiObject)
+        }
+    }
+
+    private suspend fun schedule(isVoid: Boolean, callable: () -> Any?) =
+        coroutineScope {
+            val future = async {
+                callable()
             }
             if (!isVoid) {
-                latch.countDown()
+                future.await()
+            } else {
+                Unit
             }
-            EmarsysIdlingResources.decrement()
         }
-        if (!isVoid) {
-            latch.await(timeout, TimeUnit.SECONDS)
-        }
-        return result
-    }
 }
